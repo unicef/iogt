b61a2814a435089bd09382a674ff3627d0946fa6
isedwards <ian@myacorn.com>
2021-03-02 09:42:12 +0000

    Remove 20 old files that are not in use to allow IoGT v2 work to commense in this repository

--- src/unicef/middleware.py
+++
@@ -1,7 +0,0 @@
-from django.conf import settings
-
-
-class ReferrerPolicyMiddleware(object):
-    def process_response(self, request, response):
-        response["Referrer-Policy"] = settings.REFERRER_POLICY
-        return response
