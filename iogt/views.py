ed150831c807f4a02db3c711b1f4275277de0c39
Ian Stride <ianstride@gmail.com>
2021-03-15 18:35:23 +0000

    really basic home page / section / article structure

--- iogt/views.py
+++
@@ -1,9 +0,0 @@
-from wagtail.core.models import Page
-from django.template.response import TemplateResponse
-
-def home(request):
-    model = {
-        'title': 'Hello',
-        'message': 'This is a meaningless message'
-    }
-    return TemplateResponse(request, 'home.html', model)
