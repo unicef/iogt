eed15de3092d28d5215da64245dcc2863bd45d2a
Ian Stride <ianstride@gmail.com>
2021-03-16 12:55:36 +0000

    basic home page / section / article structure (#43)
    really basic home page / section / article structure
    
    during early development the model will change a lot, so it is better to
    omit migrations until the model has settled a little.

--- home/models.py
+++ home/models.py
@@ -1,7 +1,28 @@
 from django.db import models
 
+from wagtail.core import blocks
 from wagtail.core.models import Page
+from wagtail.core.fields import StreamField
+from wagtail.admin.edit_handlers import StreamFieldPanel
+from wagtail.images.blocks import ImageChooserBlock
 
 
 class HomePage(Page):
     pass
+
+class Section(Page):
+    pass
+
+class Article(Page):
+    body = StreamField([
+        ('heading', blocks.CharBlock(form_classname="full title")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+        ('list', blocks.ListBlock(blocks.CharBlock(label="Item"))),
+        ('numbered_list', blocks.ListBlock(blocks.CharBlock(label="Item"))),
+        ('page', blocks.PageChooserBlock()),
+    ])
+
+    content_panels = Page.content_panels + [
+        StreamFieldPanel('body')
+    ]
