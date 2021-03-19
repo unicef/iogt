da3766f0875f2b9b34f2aaf103639383834065f9
Ian Stride <ianstride@gmail.com>
2021-03-19 14:08:49 +0000

    get first footer, if it exists

--- home/templatetags/home_tags.py
+++ home/templatetags/home_tags.py
@@ -6,6 +6,6 @@ register = template.Library()
 @register.inclusion_tag('home/tags/footer.html', takes_context=True)
 def footer(context):
     return {
-        'footer': Footer.objects.all()[0],
+        'footer': Footer.objects.first(),
         'request': context['request'],
     }
