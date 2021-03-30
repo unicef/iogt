dd4926d916f1952070ea73bcdb30abb46a04cc6b
Michael Clapham <michaeljclapham@gmail.com>
2021-03-30 12:23:57 +0100

    progress

--- home/templatetags/home_tags.py
+++ home/templatetags/home_tags.py
@@ -1,7 +1,13 @@
 from django import template
 from home.models import Footer
+import datetime
+import json
 
 register = template.Library()
+
+@register.simple_tag(takes_context=True)
+def dump_context(context: template.RequestContext):
+    return context.request
 
 @register.inclusion_tag('home/tags/footer.html', takes_context=True)
 def footer(context):
