6f2594f3a78179147252a1c930179102d2bbd0ed
MySecoundLanguage <mmamunism@gmail.com>
2021-05-06 10:09:04 +0600

    Added commet and flag models for Article Page

--- home/models.py
+++ home/models.py
@@ -1,12 +1,19 @@
 from django.db import models
+from django.conf import settings
+from django.utils.translation import gettext_lazy as _
+
 
 from wagtail.core import blocks
-from wagtail.core.models import Page
+from wagtail.core.models import Orderable, Page
 from wagtail.core.fields import StreamField
-from wagtail.admin.edit_handlers import StreamFieldPanel, FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, PageChooserPanel, StreamFieldPanel
+from wagtail.snippets.edit_handlers import SnippetChooserPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.images.blocks import ImageChooserBlock
 from wagtail.snippets.models import register_snippet
+from modelcluster.fields import ParentalKey
+from wagtail.admin import widgets
+
 
 
 class HomePage(Page):
@@ -70,7 +77,8 @@ class Article(Page):
 
     content_panels = Page.content_panels + [
         ImageChooserPanel('lead_image'),
-        StreamFieldPanel('body')
+        StreamFieldPanel('body'),
+        InlinePanel('comment', label="Comments"),
     ]
 
     def get_context(self, request):
@@ -84,6 +92,87 @@ class Article(Page):
             if block.block_type == 'paragraph':
                 return block
         return ''
+
+@register_snippet
+class Comment(Orderable):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        blank=True,
+        null=True,
+        related_name="comments",
+        on_delete=models.SET_NULL
+    )
+    page = ParentalKey('Article', related_name='comment')
+    author_website = models.URLField(_("author's Website"), blank=True)
+    comment = models.TextField(_('comment'), max_length=3000)
+
+    # Metadata
+    submitted_at = models.DateTimeField(
+        _('date/time submitted'),
+        default=None,
+        db_index=True
+    )
+    ip_address = models.GenericIPAddressField(_('IP address'), blank=True, null=True)
+    is_approved = models.BooleanField(
+        _('is approved'),
+        default=True,
+    )
+    is_removed = models.BooleanField(
+        _('is removed'),
+        default=False,
+        db_index=True,
+    )
+
+    date_widget = widgets.AdminDateInput(
+        attrs = {
+            'placeholder': 'dd-mm-yyyy'
+        }
+    )
+
+    panels = [
+        SnippetChooserPanel('author'),
+        PageChooserPanel('page'),
+        FieldPanel('author_website'),
+        FieldPanel('comment'),
+        FieldPanel('submitted_at', widget=date_widget),
+        FieldPanel('is_approved'),
+        FieldPanel('is_removed'),
+        FieldPanel('ip_address'),
+
+    ]
+
+
+    def __str__(self):
+        return self.comment
+
+@register_snippet
+class FlagComment(models.Model):
+    flagger = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        verbose_name=_('flagger'),
+        on_delete=models.CASCADE,
+        related_name="comment_flags",
+    )
+    comment = models.ForeignKey(
+        Comment,
+        verbose_name=_('comment'),
+        on_delete=models.CASCADE,
+        related_name="comment_flags",
+    )
+    flag = models.CharField( # Can be enum from client side
+        _('flag'),
+        max_length=30,
+        db_index=True
+    )
+    flagged_at = models.DateTimeField(_('flag date time'), default=None)
+
+    class Meta:
+        unique_together = [('flagger', 'comment', 'flag')]
+        verbose_name = _('comment flag')
+        verbose_name_plural = _('comment flags')
+
+    def __str__(self):
+        return self.flag
 
 @register_snippet
 class Footer(models.Model):
