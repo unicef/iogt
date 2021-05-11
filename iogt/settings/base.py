ee0816dd21cee98d6db8cd5a66b4449ecb886565
MySecoundLanguage <mmamunism@gmail.com>
2021-05-11 09:11:46 +0600

    added default login urls

--- iogt/settings/base.py
+++ iogt/settings/base.py
@@ -164,3 +164,5 @@ WAGTAIL_SITE_NAME = "iogt"
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 BASE_URL = 'http://example.com'
+
+LOGIN_URL = '/admin/login/'
