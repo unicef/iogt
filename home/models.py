b762b6d2b31f214aeaea153deb1167381ec72a38
djangodeveloper96 <djangodeveloper961018@gmail.com>
2021-05-20 21:18:00 +0800

    showing results.

--- home/models.py
+++ home/models.py
@@ -189,6 +189,36 @@ class FormPage(AbstractEmailForm):
             page=self, user=form.user
         )
 
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request, *args, **kwargs)
+
+        results = dict()
+        data_fields = [
+            (field.clean_name, field.label)
+            for field in self.get_form_fields()
+        ]
+
+        submissions = self.get_submission_class().objects.filter(page=self)
+        for submission in submissions:
+            data = submission.get_data()
+
+            for name, label in data_fields:
+                answer = data.get(name)
+                if answer is None:
+                    continue
+
+                if type(answer) is list:
+                    answer = u', '.join(answer)
+
+                question_stats = results.get(label, {})
+                question_stats[answer] = question_stats.get(answer, 0) + 1
+                results[label] = question_stats
+
+        context.update({
+            'results': results,
+        })
+        return context
+
 
 class CustomFormSubmission(AbstractFormSubmission):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
