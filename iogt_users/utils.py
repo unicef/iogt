e5bdaf95928ff242e341f4bab09b139f6e7023a6
Ehmad Zubair <ezubair@gmail.com>
2021-05-22 02:01:58 +0500

    Added comment for surveys

--- iogt_users/utils.py
+++ iogt_users/utils.py
@@ -2,6 +2,8 @@ from wagtail.users.forms import standard_fields
 
 
 def has_completed_registration_survey(user):
+    # TODO: Check if survey exists when surveys go live. This method should always return
+    #  True if there is no post-registration survey
     return user.has_filled_registration_survey
 
 
