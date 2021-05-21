4ef19a9d83d4a32f7fd3ebacc5d28b48c92ab4d9
djangodeveloper96 <djangodeveloper961018@gmail.com>
2021-05-21 22:33:22 +0800

    Added Positive Number field type.

--- polls/models.py
+++ polls/models.py
@@ -1,8 +1,11 @@
 import json
+from django import forms
 from django.db import models
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel, FieldRowPanel, InlinePanel, MultiFieldPanel
-from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField, AbstractFormSubmission
+from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField, AbstractFormSubmission, \
+    FORM_FIELD_CHOICES
+from wagtail.contrib.forms.forms import FormBuilder
 from modelcluster.fields import ParentalKey
 
 from django.conf import settings
@@ -11,7 +14,20 @@ from django.shortcuts import render
 
 
 class FormField(AbstractFormField):
+    CHOICES = FORM_FIELD_CHOICES + (('positivenumber', 'Positive number'),)
+
     page = ParentalKey('PollPage', on_delete=models.CASCADE, related_name='form_fields')
+
+    field_type = models.CharField(
+        verbose_name='field type',
+        max_length=16,
+        choices=CHOICES
+    )
+
+
+class CustomFormBuilder(FormBuilder):
+    def create_positivenumber_field(self, field, options):
+        return forms.NumberInput(**options)
 
 
 class PollPage(AbstractEmailForm):
@@ -35,6 +51,8 @@ class PollPage(AbstractEmailForm):
             FieldPanel('subject'),
         ], "Email"),
     ]
+
+    form_builder = CustomFormBuilder
 
     def serve(self, request, *args, **kwargs):
         if self.get_submission_class().objects.filter(page=self, user__pk=request.user.pk).exists():
