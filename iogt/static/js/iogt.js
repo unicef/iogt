af2c4eb25b19dc366f376de631adb3537eb730d8
Danny Parsons <dannyparsons@users.noreply.github.com>
2021-05-29 22:15:44 +0100

    Edited warning message

--- iogt/static/js/iogt.js
+++ iogt/static/js/iogt.js
@@ -6,7 +6,7 @@ function validateFileUpload(fileInput) {
     if (file.size >= 9437184)
       return confirm('The file you have uploaded exceeds 9mb. This will prohibit access to the file in a low ' +
           'bandwidth setting, may restrict feature phone access, or violate your mobile network operator ' +
-          'agreements. To reduce file size, try resizing and compressing your file.');
+          'agreements. To reduce file size, try resizing and compressing your file. Do you want to contnue?');
   }
 
   return true;
