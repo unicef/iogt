08786aa9b69463a031fab1ea0241d903f7fb4967
Ehmad Zubair <ezubair@gmail.com>
2021-05-31 21:50:44 +0500

    - Added CSS - Copied and Edited Templates from django-comments-xtd - Updated comment form

--- comments/forms.py
+++ comments/forms.py
@@ -3,6 +3,13 @@ from django_comments_xtd.forms import XtdCommentForm as BaseCommentForm
 
 
 class CommentForm(BaseCommentForm):
+
     def __init__(self, *args, **kwargs):
         super(CommentForm, self).__init__(*args, **kwargs)
-        # self.fields['email'].widget = forms.HiddenInput()
+        self.fields['email'].initial = 'noreply@iogt.com'
+        self.fields['email'].widget = forms.HiddenInput()
+
+        self.fields['followup'].initial = False
+        self.fields['followup'].widget = forms.HiddenInput()
+        # followup
+
--- iogt/settings/base.py
+++ iogt/settings/base.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'home',
     'search',
     'iogt_users',
+    'comments',
 
     'wagtail.contrib.forms',
     'wagtail.contrib.redirects',
@@ -217,4 +218,13 @@ COMMENTS_XTD_CONTACT_EMAIL = "helpdesk@example.com"
 COMMENTS_XTD_CONFIRM_EMAIL = False
 
 COMMENTS_XTD_FORM_CLASS = 'comments.forms.CommentForm'
+
+COMMENTS_XTD_APP_MODEL_OPTIONS = {
+    'default': {
+        'allow_flagging': True,
+        'allow_feedback': True,
+        'show_feedback': True,
+        'who_can_post': 'users'
+    }
+}
 
