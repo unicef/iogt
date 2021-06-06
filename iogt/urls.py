ac44b5f9ed91ab56451e66eb975e60e95a2e33d1
Mikolaj Paczkowski <m.paczkowski@merixstudio.com>
2021-06-06 13:35:12 +0200

    IU-12 | move adding django debug toolbar to PR #93

--- iogt/urls.py
+++ iogt/urls.py
@@ -21,16 +21,12 @@ urlpatterns = [
 
 
 if settings.DEBUG:
-    import debug_toolbar
     from django.conf.urls.static import static
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
     # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-    # add django debug toolbar links
-    urlpatterns = urlpatterns + [path(r"__debug__/", include(debug_toolbar.urls))]
 
 urlpatterns = urlpatterns + [
     # For anything not caught by a more specific rule above, hand over to
