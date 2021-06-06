7bb4b6e5db6c5e685f23ba76e246044594358480
Ian Edwards <isedwards@users.noreply.github.com>
2021-06-06 20:33:32 +0100

    Update dev.py
    Add missing comma to INTERNAL_IPS tuple (the missing comma didn't seem to cause an errors when testing but certainly needs to be there)

--- iogt/settings/dev.py
+++ iogt/settings/dev.py
@@ -15,7 +15,7 @@ if DEBUG:
     INSTALLED_APPS += ("debug_toolbar",)
     MIDDLEWARE += ("debug_toolbar.middleware.DebugToolbarMiddleware",)
 
-    INTERNAL_IPS = ("127.0.0.1")
+    INTERNAL_IPS = ("127.0.0.1",)
     DEBUG_TOOLBAR_CONFIG = {
         "INTERCEPT_REDIRECTS": False,
         "SHOW_TOOLBAR_CALLBACK": lambda *x: True,
