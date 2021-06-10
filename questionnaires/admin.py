786f1c2983062043cafe3eaf2c0f68557f14bfa9
Mikolaj Paczkowski <m.paczkowski@merixstudio.com>
2021-06-10 17:53:04 +0200

    IU-4 | add basic poll model

--- questionnaires/admin.py
+++ questionnaires/admin.py
@@ -1,3 +1,18 @@
-from django.contrib import admin
+from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register
 
-# Register your models here.
+from .models import Poll
+
+
+class PollPageAdmin(ModelAdmin):
+
+    model = Poll
+    menu_label = "Polls"
+    menu_icon = "placeholder"
+    menu_order = 200
+    add_to_settings_menu = False
+    exclude_from_explorer = False
+    list_display = ("title",)
+    search_fields = ("title",)
+
+
+modeladmin_register(PollPageAdmin)
