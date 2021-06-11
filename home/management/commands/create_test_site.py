4f0c2008aae223ed3a65bcee27f1d8e619f7c70a
Sher Ali <sheralim012@gmail.com>
2021-06-11 10:33:32 +0500

    adds fixture for banner, and feature content and banner on homepage

--- home/management/commands/create_test_site.py
+++ home/management/commands/create_test_site.py
@@ -1,8 +1,13 @@
 from datetime import datetime, timezone
+from io import BytesIO
 
+import requests
 from django.contrib.auth import get_user_model
+from django.core.files.images import ImageFile
 from django.core.management.base import BaseCommand
 from wagtail.core.rich_text import RichText
+from wagtail.images.models import Image
+
 import home.models as models
 
 User = get_user_model()
@@ -11,6 +16,8 @@ User = get_user_model()
 class Command(BaseCommand):
 
     def clear(self):
+        models.BannerPage.objects.all().delete()
+        models.BannerIndexPage.objects.all().delete()
         models.Article.objects.all().delete()
         models.Section.objects.all().delete()
 
@@ -34,6 +41,22 @@ class Command(BaseCommand):
         home.add_child(instance=youth)
         youth.add_child(instance=internet_safety)
         internet_safety.add_child(instance=article)
+
+        models.FeaturedContent.objects.create(source=home, content=youth)
+
+        banner_index_page = models.BannerIndexPage(title='Banners')
+        home.add_child(instance=banner_index_page)
+
+        image_url = 'https://via.placeholder.com/729x576.png?text=Youth'
+        http_res = requests.get(image_url)
+        title = 'youth_banner.jpg'
+        image_file = ImageFile(BytesIO(http_res.content), name=title)
+        image = Image.objects.create(title=title, file=image_file)
+
+        banner_page = models.BannerPage(title='Youth', banner_image=image, banner_link_page=youth)
+        banner_index_page.add_child(instance=banner_page)
+
+        models.HomePageBanner.objects.create(source=home, banner_page=banner_page)
 
     def handle(self, *args, **options):
         self.clear()
