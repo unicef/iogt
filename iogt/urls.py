925688d2b3e1539f14cb7cd77a68e57ecc8dd08e
Mikolaj Paczkowski <m.paczkowski@merixstudio.com>
2021-06-13 20:30:38 +0200

    IU-4 | add stuff for transition page (external links)

--- iogt/urls.py
+++ iogt/urls.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+
+from iogt.views import TransitionPageView
 from iogt_users import urls as users_urls
 from search import views as search_views
 from wagtail.admin import urls as wagtailadmin_urls
@@ -8,15 +10,15 @@ from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
 urlpatterns = [
-    path('django-admin/', admin.site.urls),
-
-    path('admin/', include(wagtailadmin_urls)),
-    path('documents/', include(wagtaildocs_urls)),
-
-    path('search/', search_views.search, name='search'),
-    path('users/', include(users_urls), name='users_urls'),
-    path('accounts/', include('allauth.urls'), name='allauth-urls'),
-    path('comments/', include('django_comments_xtd.urls')),
+    path("django-admin/", admin.site.urls),
+    path("admin/", include(wagtailadmin_urls)),
+    path("documents/", include(wagtaildocs_urls)),
+    path("search/", search_views.search, name="search"),
+    path("users/", include(users_urls), name="users_urls"),
+    path("accounts/", include("allauth.urls"), name="allauth-urls"),
+    path("comments/", include("django_comments_xtd.urls")),
+    path("question/", include("questionnaires.urls"), name="question"),
+    path("external-link/", TransitionPageView.as_view(), name="external-link"),
 ]
 
 if settings.DEBUG:
@@ -36,7 +38,6 @@ urlpatterns = urlpatterns + [
     # Wagtail's page serving mechanism. This should be the last pattern in
     # the list:
     path("", include(wagtail_urls)),
-
     # Alternatively, if you want Wagtail pages to be served from a subpath
     # of your site, rather than the site root:
     #    path("pages/", include(wagtail_urls)),
