26b15e5593e8a2a59806900c005e860e56278c62
Mikolaj Paczkowski <m.paczkowski@merixstudio.com>
2021-06-13 20:23:54 +0200

    IU-4 | change models for questionnaires

--- questionnaires/models.py
+++ questionnaires/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from home.blocks import MediaBlock
+from home.models import HomePage
 from iogt.views import create_final_external_link
 from iogt_users.models import User
 from wagtail.admin.edit_handlers import (
@@ -13,7 +14,6 @@ from wagtail.core import blocks
 from wagtail.core.fields import StreamField
 from wagtail.core.models import Page
 from wagtail.images.blocks import ImageChooserBlock
-from wagtail.images.edit_handlers import ImageChooserPanel
 
 
 class QuestionnairePage(Page):
@@ -21,40 +21,22 @@ class QuestionnairePage(Page):
     parent_page_types = []
     subpage_types = []
 
-    image = models.ForeignKey(
-        "wagtailimages.Image",
-        on_delete=models.PROTECT,
-        blank=True,
-        null=True,
-        related_name="survey_image",
-    )
     description = StreamField(
         [
-            ("heading", blocks.CharBlock(classname="full title")),
-            ("paragraph", blocks.TextBlock()),
+            ("paragraph", blocks.RichTextBlock()),
             ("image", ImageChooserBlock()),
-            ("list", blocks.ListBlock(blocks.CharBlock(label="Item"))),
-            ("numbered_list", blocks.ListBlock(blocks.CharBlock(label="Item"))),
         ],
         null=True,
         blank=True,
     )
     thank_you_text = StreamField(
         [
-            ("heading", blocks.CharBlock(classname="full title")),
-            ("paragraph", blocks.TextBlock()),
+            ("paragraph", blocks.RichTextBlock()),
             ("media", MediaBlock(icon="media")),
+            ("image", ImageChooserBlock()),
         ],
         null=True,
         blank=True,
-    )
-    thank_you_image = models.ForeignKey(
-        "wagtailimages.Image",
-        on_delete=models.PROTECT,
-        blank=True,
-        null=True,
-        related_name="survey_thank_you_image",
-        help_text=_("Add image to thank you page"),
     )
     thank_you_link_text = models.CharField(
         max_length=40,
@@ -79,19 +61,12 @@ class QuestionnairePage(Page):
         on_delete=models.PROTECT,
         help_text=_("Optional page to which user after questions can go"),
     )
-
     allow_anonymous_submissions = models.BooleanField(
         default=False,
         help_text=_(
             "Check this to allow users who are NOT logged in to complete surveys."
         ),
     )
-
-    def __str__(self):
-        return self.title
-
-    class Meta:
-        abstract = True
 
     @property
     def thank_you_link(self):
@@ -102,10 +77,15 @@ class QuestionnairePage(Page):
         else:
             return "#"
 
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        abstract = True
+
 
 class PollIndexPage(Page):
-    template = None
-    parent_page_types = ["home.HomePage"]
+    parent_page_types = ["home.HomePage", "home.Section", "home.Article"]
     subpage_types = ["questionnaires.Poll"]
 
 
@@ -139,7 +119,6 @@ class Poll(QuestionnairePage):
     content_panels = Page.content_panels + [
         MultiFieldPanel(
             [
-                StreamFieldPanel("description"),
                 FieldPanel("allow_anonymous_submissions"),
                 FieldPanel("show_results"),
                 FieldPanel("randomise_options"),
@@ -147,33 +126,52 @@ class Poll(QuestionnairePage):
                 FieldPanel("allow_multiple_choice"),
             ],
             heading=_(
-                "Start info Settings",
+                "General settings for poll",
+            ),
+        ),
+        MultiFieldPanel(
+            [
+                StreamFieldPanel("description"),
+            ],
+            heading=_(
+                "Description at poll page",
             ),
         ),
         MultiFieldPanel(
             [
                 StreamFieldPanel("thank_you_text"),
+            ],
+            heading="Description at thank you page",
+        ),
+        MultiFieldPanel(
+            [
                 FieldPanel("thank_you_link_text"),
                 PageChooserPanel("thank_you_internal_link"),
                 FieldPanel("thank_you_external_link"),
-                ImageChooserPanel("thank_you_image"),
             ],
-            heading="Thank you page settings",
+            heading="Link at  thank you page",
         ),
     ]
+
+    def choices(self):
+        if self.randomise_options:
+            return Choice.objects.live().child_of(self).order_by("?")
+        return Choice.objects.live().child_of(self)
+
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request, *args, **kwargs)
+        context["choices"] = self.choices
+        return context
+
+    def __str__(self):
+        return self.title
 
     class Meta:
         verbose_name = "Poll"
         verbose_name_plural = "Polls"
 
-    def get_context(self, request):
-        context = super().get_context(request)
-        context["choices"] = self.get_children().type(Choice)
-        return context
-
 
 class Choice(Page):
-    template = None
     parent_page_types = ["questionnaires.Poll"]
     subpage_types = []
 
@@ -181,6 +179,13 @@ class Choice(Page):
     choice_votes = models.ManyToManyField(
         "ChoiceVote", related_name="choices", blank=True
     )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Choice"
+        verbose_name_plural = "Choices"
 
 
 class ChoiceVote(models.Model):
@@ -192,5 +197,9 @@ class ChoiceVote(models.Model):
         blank=True,
     )
     choice = models.ManyToManyField(Choice, blank=True)
-    poll = models.ForeignKey(Poll, on_delete=models.SET_NULL, null=True)
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE, null=True)
     submission_date = models.DateField(null=True, blank=True, auto_now_add=True)
+
+    @property
+    def answer(self):
+        return ",".join([c.title for c in self.choice.all()])
