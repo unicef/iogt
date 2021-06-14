98189a213f186fe1c4c8dc5117e33991380d855e
Ehmad Zubair <ezubair@gmail.com>
2021-06-14 19:21:49 +0500

    Added Readme steps

--- iogt/settings/test.py
+++ iogt/settings/test.py
@@ -1,4 +1,5 @@
 from .base import *
+
 DEBUG = True
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'jjfayj6d=90@@@(rop$98ryt36vuyf3!chtneyoku3_f)*z^h_'
@@ -6,6 +7,8 @@ SECRET_KEY = 'jjfayj6d=90@@@(rop$98ryt36vuyf3!chtneyoku3_f)*z^h_'
 ALLOWED_HOSTS = ['*']
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # try:
 #     from .local import *
