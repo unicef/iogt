9f3f29af99a967204d5acf3052100012a7daebe0
Mikolaj Paczkowski <m.paczkowski@merixstudio.com>
2021-06-15 07:47:44 +0200

    IU-12 | linter

--- home/tests/test_links_in_page/test_external_links.py
+++ home/tests/test_links_in_page/test_external_links.py
@@ -1,11 +1,10 @@
 from bs4 import BeautifulSoup
-from django.test import TestCase, Client
+from django.test import Client, TestCase
+from home.models import Article
+from home.tests.faker import faker
 from rest_framework import status
 from wagtail.core.models import Page
 from wagtail.core.rich_text import RichText
-
-from home.models import Article
-from home.tests.faker import faker
 
 
 class TransitionArticleForExternalLinksTests(TestCase):
@@ -24,12 +23,24 @@ class TransitionArticleForExternalLinksTests(TestCase):
 
     def test_if_rich_text_field_external_links_redirect_to_transition_page(self):
         body = [
-            ("paragraph",
-             RichText('<p><a href="https://aynthing.com">external link at the beginning</a> dolor sit amet</p>')),
-            ("paragraph",
-             RichText('<p>Lorem ipsum <a href="https://aynthing.com">external link in middle</a> dolor sit amet</p>')),
-            ("paragraph",
-             RichText('<p>Lorem dolor sit amet <a href="https://aynthing.com">external link at the end</a></p>')),
+            (
+                "paragraph",
+                RichText(
+                    '<p><a href="https://aynthing.com">external link at the beginning</a> dolor sit amet</p>'
+                ),
+            ),
+            (
+                "paragraph",
+                RichText(
+                    '<p>Lorem ipsum <a href="https://aynthing.com">external link in middle</a> dolor sit amet</p>'
+                ),
+            ),
+            (
+                "paragraph",
+                RichText(
+                    '<p>Lorem dolor sit amet <a href="https://aynthing.com">external link at the end</a></p>'
+                ),
+            ),
         ]
 
         article = self.create_published_article_to_root(body=body)
@@ -45,15 +56,26 @@ class TransitionArticleForExternalLinksTests(TestCase):
         for link in main_block.find_all("a"):
             assert self.external_link_pattern in link["href"]
 
-
     def test_if_rich_text_field_internal_links_redirect_directly(self):
         body = [
-            ("paragraph",
-             RichText('<p><a id="3" linktype="page">internal link at the beginning</a> dolor sit amet</p>')),
-            ("paragraph",
-             RichText('<p>Lorem ipsum <a id="3" linktype="page">internal link in middle</a> dolor sit amet</p>')),
-            ("paragraph",
-             RichText('<p>Lorem dolor sit amet <a id="3" linktype="page">external link at the end</a></p>')),
+            (
+                "paragraph",
+                RichText(
+                    '<p><a id="3" linktype="page">internal link at the beginning</a> dolor sit amet</p>'
+                ),
+            ),
+            (
+                "paragraph",
+                RichText(
+                    '<p>Lorem ipsum <a id="3" linktype="page">internal link in middle</a> dolor sit amet</p>'
+                ),
+            ),
+            (
+                "paragraph",
+                RichText(
+                    '<p>Lorem dolor sit amet <a id="3" linktype="page">external link at the end</a></p>'
+                ),
+            ),
         ]
 
         article = self.create_published_article_to_root(body=body)
