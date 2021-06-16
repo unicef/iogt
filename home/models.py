2cc4d62e386ad5b6a8f7cb33edfc57670cc759a6
Sher Ali <sheralim012@gmail.com>
2021-06-16 22:13:41 +0500

    removes unused fields, updated file size usage and fixed migrations

--- home/models.py
+++ home/models.py
@@ -278,22 +278,6 @@ class SiteSettings(BaseSetting):
             "Global GA tracking code to be used"
             " to view analytics on more than one site globally")
     )
-    content_rotation_start_date = models.DateTimeField(
-        null=True, blank=True,
-        help_text='The date rotation will begin')
-    content_rotation_end_date = models.DateTimeField(
-        null=True, blank=True,
-        help_text='The date rotation will end')
-    monday_rotation = models.BooleanField(default=False, verbose_name='Monday')
-    tuesday_rotation = models.BooleanField(default=False, verbose_name='Tuesday')
-    wednesday_rotation = models.BooleanField(default=False, verbose_name='Wednesday')
-    thursday_rotation = models.BooleanField(default=False, verbose_name='Thursday')
-    friday_rotation = models.BooleanField(default=False, verbose_name='Friday')
-    saturday_rotation = models.BooleanField(default=False, verbose_name='Saturday')
-    sunday_rotation = models.BooleanField(default=False, verbose_name='Sunday')
-    time = StreamField([
-        ('time', blocks.TimeBlock(required=False)),
-    ], null=True, blank=True, help_text='The time/s content will be rotated')
     social_media_link = StreamField([
         ('social_media_link', SocialMediaLinkBlock()),
     ], null=True, blank=True)
@@ -301,7 +285,8 @@ class SiteSettings(BaseSetting):
         ('social_media_content_sharing_button', SocialMediaShareButtonBlock()),
     ], null=True, blank=True)
     media_file_size_threshold = models.IntegerField(
-        default=9, help_text='Show warning if uploaded media file size is greater than this in MB.')
+        default=9437184,
+        help_text='Show warning if uploaded media file size is greater than this in bytes. Default is 9 MB')
     allow_anonymous_comment = models.BooleanField(default=False)
 
     panels = [
@@ -332,22 +317,6 @@ class SiteSettings(BaseSetting):
             ],
             heading="GA Tracking Code Settings",
         ),
-        MultiFieldPanel(
-            [
-                FieldPanel('content_rotation_start_date'),
-                FieldPanel('content_rotation_end_date'),
-                FieldRowPanel([
-                    FieldPanel('monday_rotation', classname='col6'),
-                    FieldPanel('tuesday_rotation', classname='col6'),
-                    FieldPanel('wednesday_rotation', classname='col6'),
-                    FieldPanel('thursday_rotation', classname='col6'),
-                    FieldPanel('friday_rotation', classname='col6'),
-                    FieldPanel('saturday_rotation', classname='col6'),
-                    FieldPanel('sunday_rotation', classname='col6'),
-                ]),
-                StreamFieldPanel('time'),
-            ],
-            heading="Content Rotation Settings", ),
         MultiFieldPanel(
             [
                 MultiFieldPanel(
--- iogt/static/js/iogt.js
+++ iogt/static/js/iogt.js
@@ -3,11 +3,11 @@ function validateFileUpload(fileInput, file_size_threshold) {
     return true;
   else {
     var file = fileInput.files[0];
-    if (Math.round(file.size / 1024 / 1024) >= file_size_threshold)
-      return confirm('The file you have uploaded exceeds ' + file_size_threshold + 'mb. This will ' +
-          'prohibit access to the file in a low bandwidth setting, may restrict feature phone access, or violate ' +
-          'your mobile network operator agreements. To reduce file size, try resizing and compressing your file. ' +
-          'Do you want to continue?');
+    if (file.size >= file_size_threshold)
+      return confirm('The file you have uploaded exceeds ' + Math.round(file_size_threshold / 1024 / 1024) + 'mb. ' +
+          'This will prohibit access to the file in a low bandwidth setting, may restrict feature phone access, or ' +
+          'violate your mobile network operator agreements. To reduce file size, try resizing and compressing your ' +
+          'file. Do you want to continue?');
   }
 
   return true;
