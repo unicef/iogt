55116d70993f56cd799827ae124ca1dd282ef7eb
Kishokanth-netizen <80698644+Kishokanth-netizen@users.noreply.github.com>
2021-06-18 17:50:23 +0200

    Workbox is now served from local assets and not cdn
    Fixed the issue with workbox being served via a CDN as this would incur costs for the end users. Assets are now served from the project

--- iogt/templates/sw.js
+++ iogt/templates/sw.js
@@ -1,11 +1,12 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.5/workbox-sw.js');
+importScripts('../static/third_party/workbox/workbox-v6.1.5/workbox-sw.js');
 
 // The below implementation will basically convert "{{ test_url }}" which is test/ into http://localhost:8000/test
 // The relative paths such as test/ of the pages inside the app will be provided by the backend devs, 
 // so this will be done programmatically
 
 const appShell = [
-  "{{ test_url }}"
+  "{{ test_url }}",
+  ""
 ].map((partialUrl) => `${location.protocol}//${location.host}${partialUrl}`);
 
 workbox.precaching.precacheAndRoute(appShell.map(url => ({
