1c31dad2a3ce520502f480188eea0bff69ec67aa
Mikolaj Paczkowski <m.paczkowski@merixstudio.com>
2021-06-21 17:10:45 +0200

    Allow linking to specific language edition of the site via URL parameters (#120)
    Closes #87.

--- iogt/urls.py
+++ iogt/urls.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import include, path
 from iogt_users import urls as users_urls
@@ -11,10 +12,8 @@ from iogt.views import TransitionPageView
 
 urlpatterns = [
     path('django-admin/', admin.site.urls),
-
     path('admin/', include(wagtailadmin_urls)),
     path('documents/', include(wagtaildocs_urls)),
-
     path('search/', search_views.search, name='search'),
     path('users/', include(users_urls), name='users_urls'),
     path('accounts/', include('allauth.urls'), name='allauth-urls'),
@@ -35,13 +34,6 @@ if settings.DEBUG:
     # add django debug toolbar links
     urlpatterns = urlpatterns + [path(r"__debug__/", include(debug_toolbar.urls))]
 
-urlpatterns = urlpatterns + [
-    # For anything not caught by a more specific rule above, hand over to
-    # Wagtail's page serving mechanism. This should be the last pattern in
-    # the list:
+urlpatterns = urlpatterns + i18n_patterns(
     path("", include(wagtail_urls)),
-
-    # Alternatively, if you want Wagtail pages to be served from a subpath
-    # of your site, rather than the site root:
-    #    path("pages/", include(wagtail_urls)),
-]
+)
