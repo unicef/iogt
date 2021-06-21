66b0ce10e10707323358af905085f61acf182497
Kishokanth-netizen <80698644+Kishokanth-netizen@users.noreply.github.com>
2021-06-21 14:05:46 +0200

    banner is displayed based on cache settings for first time users

--- iogt/static/js/home.js
+++ iogt/static/js/home.js
@@ -2,6 +2,7 @@
 const displayBanner =()=> {
 if (confirm("Do you want to save changes?") === true) {
    console.log("PWA saved successfully!")
+   cache()
 } else {
   console.log("PWA not downloaded!")
 }
