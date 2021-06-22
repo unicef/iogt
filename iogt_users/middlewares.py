4460c596cc3bfea7d0a4f1401c1f7e21b2843a64
Ehmad Zubair <ezubair@gmail.com>
2021-06-22 13:21:40 +0500

    Updated middleware to allow logout url

--- iogt_users/middlewares.py
+++ iogt_users/middlewares.py
@@ -13,11 +13,12 @@ class RegistrationSurveyRedirectMiddleware:
 
     def __call__(self, request):
         current_url = resolve(request.path_info).url_name
-        is_post_registration_survey_url = current_url == 'post_registration_survey'
+        allowed_url_names = ['post_registration_survey', 'account_logout']
+
         is_registered_user = not request.user.is_anonymous
 
-        if is_registered_user and not is_post_registration_survey_url and \
-                not request.user.has_filled_registration_survey:
+        if is_registered_user and not request.user.has_filled_registration_survey and\
+                current_url not in allowed_url_names:
             return redirect('post_registration_survey')
 
         return self.get_response(request)
