f35760b15fb82323acc4b77d87df637cc783ae96
Sher Ali <sheralim012@gmail.com>
2021-06-23 12:01:51 +0500

    fixed delete thread view

--- messaging/urls.py
+++ messaging/urls.py
@@ -7,8 +7,8 @@ app_name = "messaging"
 
 urlpatterns = [
     path('api/', include(api_urls)),
-    path('inbox', views.InboxView.as_view(), name="inbox"),
+    path('inbox/', views.InboxView.as_view(), name="inbox"),
     path('create/', views.MessageCreateView.as_view(), name="message_create"),
-    path('thread/<int:pk>', views.ThreadView.as_view(), name="thread_detail"),
-    path('thread/<int:pk>/delete', views.ThreadDeleteView.as_view(), name="thread_delete"),
+    path('thread/<int:pk>/', views.ThreadView.as_view(), name="thread_detail"),
+    path('thread/<int:pk>/delete/', views.ThreadDeleteView.as_view(), name="thread_delete"),
 ]
