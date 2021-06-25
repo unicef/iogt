afe6ce1d74819b9a75f474138d0c47787b4959e5
Kishokanth-netizen <80698644+Kishokanth-netizen@users.noreply.github.com>
2021-06-25 16:28:40 +0200

    Add 'offline mode' via Workbox (#110)
    User can now click on download website button to cache the content that we deem fit. Back-end devs will support this by providing sitemap-style list of URLs for the content that will be cached.
    
    
    Co-authored-by: Ian Stride <ianstride@gmail.com>

---
+++ iogt/templates/sw.js
@@ -0,0 +1,15 @@
+importScripts('../static/third_party/workbox/workbox-v6.1.5/workbox-sw.js');
+
+// The below implementation will basically convert "{{ test_url }}" which is test/ into http://localhost:8000/test
+// The relative paths such as test/ of the pages inside the app will be provided by the backend devs, 
+// so this will be done programmatically
+
+const appShell = [
+  "{{ test_url }}",
+].map((partialUrl) => `${location.protocol}//${location.host}${partialUrl}`);
+
+workbox.precaching.precacheAndRoute(appShell.map(url => ({
+  url,
+  revision: null,
+})));
+
