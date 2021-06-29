eaf6eb6327dd1b9c64cc4e6372b965e6f8d6c333
Sher Ali <sheralim012@gmail.com>
2021-06-29 15:18:55 +0500

    adds registration survey response in user export list

--- iogt_users/wagtail_hooks.py
+++ iogt_users/wagtail_hooks.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register, ModelAdminGroup
 
+from home.models import SiteSettings
 from iogt_users.filters import GroupsFilter
 
 
@@ -13,10 +14,16 @@ class UsersExportAdmin(ModelAdmin):
     form_fields_exclude = ('password', 'last_login', 'is_superuser', 'groups', 'user_permissions')
     search_fields = ('username',)
     list_export = ('username', 'first_name', 'last_name', 'email', 'is_staff', 'is_active', 'date_joined',
-                   'terms_accepted', 'has_filled_registration_survey')
+                   'terms_accepted', 'has_filled_registration_survey', 'registration_survey_response',)
     add_to_settings_menu = True
     list_per_page = 20
     menu_order = 1001
+
+    def registration_survey_response(self, obj):
+        site_settings = SiteSettings.get_for_default_site()
+        user_submission = obj.usersubmission_set.filter(
+            page__pk=site_settings.registration_survey.pk).order_by('-submit_time').first()
+        return user_submission.form_data if user_submission else ''
 
 
 class ExportGroup(ModelAdminGroup):
