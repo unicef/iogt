97f87c839f4f5aefa47d8db9720d639695139a93
Kishokanth-netizen <80698644+Kishokanth-netizen@users.noreply.github.com>
2021-06-30 17:24:22 +0200

    cleaned SW

--- iogt/templates/sw.js
+++ iogt/templates/sw.js
@@ -1,5 +1,6 @@
 importScripts('../static/third_party/workbox/workbox-v6.1.5/workbox-sw.js');
 
+
 // Below we are using a custom dimension to track online vs. offline interactions. So make
 // sure to create a custom dimension on GA
 
@@ -13,15 +14,13 @@ workbox.googleAnalytics.initialize({
 // The relative paths such as test/ of the pages inside the app will be provided by the backend devs,
 // so this will be done programmatically
 
+
 const appShell = [
   "/youth",
+  '/'
 ].map((partialUrl) => `${location.protocol}//${location.host}${partialUrl}`);
 
 workbox.precaching.precacheAndRoute(appShell.map(url => ({
   url,
   revision: null,
 })));
-
-const successMsg = "Your app is now ready to install. If you are using a iOS device, you can install it by clicking 'Share', scrolling down and tapping 'Add to Home Screen. If using Android choose 'Add to home screen' and you should be all set!";
-
-alert(successMsg)
