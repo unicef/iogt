00304d48663ebcee4481a58d31fa6856555840c7
Fourmetadev <fourmetadev@gmail.com>
2021-07-01 17:29:08 +0300

    Add dynamic languages

--- home/templatetags/home_tags.py
+++ home/templatetags/home_tags.py
@@ -1,6 +1,7 @@
 from django import template
 
 from home.models import FooterPage, SectionIndexPage
+from iogt.settings.base import LANGUAGES
 
 register = template.Library()
 
@@ -55,6 +56,15 @@ def render_questionnaire_list(questionnaire):
 def render_user_progress(user_progress):
     return user_progress
 
+
 @register.inclusion_tag('home/tags/sub_sections.html')
 def render_sub_sections_list(sub_sections):
     return {'sub_sections': sub_sections}
+
+
+@register.simple_tag
+def locale_set(locale, url):
+    for item in LANGUAGES:
+        code = item[0]
+        url = url.replace(f"/{code}/", "")
+    return f'/{locale}/{url}'
