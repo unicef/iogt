9d344b6008fb85052e473958a7fc19309217cd4e
Sher Ali <sheralim012@gmail.com>
2021-07-01 17:40:43 +0500

    adds wagtail svg support

--- iogt/settings/base.py
+++ iogt/settings/base.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'wagtailmedia',
     'wagtailmarkdown',
     'wagtail_transfer',
+    'wagtailsvg',
     'wagtail.contrib.settings',
 
     'django_comments_xtd',
@@ -69,7 +70,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites'
+    'django.contrib.sites',
 ]
 
 MIDDLEWARE = [
