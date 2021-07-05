abb328e1e0f2962fb0016294933817cac767333e
Fourmetadev <fourmetadev@gmail.com>
2021-07-05 18:08:33 +0300

    Make markup for checkboxes and text inputs

--- questionnaires/templatetags/questionnaires_tags.py
+++ questionnaires/templatetags/questionnaires_tags.py
@@ -8,14 +8,15 @@ def render_radios(field):
     return {'field': field}
 
 
-@register.inclusion_tag('questionnaires/tags/polls_list.html')
-def render_polls_list(polls):
-    return {'polls': polls}
+@register.inclusion_tag('questionnaires/tags/checkboxes.html')
+def render_checkboxes(field):
+    print(field)
+    return {'field': field}
 
 
-@register.inclusion_tag('questionnaires/tags/quizzes_list.html')
-def render_quizzes_list(quizzes):
-    return {'quizzes': quizzes}
+@register.inclusion_tag('questionnaires/tags/text_field.html')
+def render_text_field(field):
+    return {'field': field}
 
 
 @register.filter
