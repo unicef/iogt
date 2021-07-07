fe05000d15572125588fa79170ef064d946022eb
Ehmad Zubair <ezubair@gmail.com>
2021-07-07 19:53:59 +0500

    Make sqlite the default database

--- iogt/settings/docker_compose_dev.py
+++ iogt/settings/docker_compose_dev.py
@@ -14,13 +14,14 @@ WAGTAILSEARCH_BACKENDS = {
         }
 }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
-    }
-}
+# Uncomment if you want to run Postgres
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get('DB_NAME'),
+#         'USER': os.environ.get('DB_USER'),
+#         'PASSWORD': os.environ.get('DB_PASSWORD'),
+#         'HOST': os.environ.get('DB_HOST'),
+#         'PORT': os.environ.get('DB_PORT'),
+#     }
+# }
