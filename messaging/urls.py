3dfd9173a952f130bc7fe18ae2981df87fe63451
Ehmad Zubair <ezubair@gmail.com>
2021-07-14 17:41:03 +0500

    Fixes a bug with chatbot create

--- messaging/urls.py
+++ messaging/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('api/', include(api_urls)),
     path('inbox/', views.InboxView.as_view(), name='inbox'),
     path('message/<int:thread_id>', views.ThreadDetailView.as_view(), name='thread'),
+    path('message/create', views.MessageCreateView.as_view(), name='message_create'),
     path('thread/<int:pk>/delete/', views.ThreadDeleteView.as_view(), name="thread_delete"),
 ]
 
