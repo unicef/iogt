be6d24288b11c061009e818e7d7a73da37cd4c12
Ehmad Zubair <ezubair@gmail.com>
2021-09-08 13:02:39 +0500

    Made user migration idempotent

--- iogt_content_migration/management/commands/load_v1_users.py
+++ iogt_content_migration/management/commands/load_v1_users.py
@@ -13,9 +13,10 @@ from pip._vendor.distlib.compat import raw_input
 from wagtail.contrib.forms.utils import get_field_clean_name
 
 from tqdm import tqdm
+from wagtail.core.models import Page
 
-from home.models import Article, SectionIndexPage
-from questionnaires.models import Survey, UserSubmission, SurveyIndexPage, Poll
+from home.models import Article, V1ToV2ObjectMap
+from questionnaires.models import Survey, UserSubmission, Poll
 
 
 class Command(BaseCommand):
@@ -63,13 +64,7 @@ class Command(BaseCommand):
         self.db_connect(options)
 
         self.registration_survey_mandatory_group_ids = self.request_registration_survey_mandatory_groups()
-
         self.content_type_map = dict()
-        self.comments_map = dict()
-        self.articles_map = dict()
-        self.surveys_map = dict()
-        self.polls_map = dict()
-        self.users_map = dict()
 
         self.delete_users = options.get('delete_users')
 
@@ -143,84 +138,14 @@ class Command(BaseCommand):
 
     def migrate(self):
         self.populate_content_type_map()
-        self.populate_articles_map()
-        self.populate_surveys_map()
 
         self.migrate_user_groups()
         self.migrate_user_accounts()
         self.mark_user_registration_survey_required()
-        self.populate_users_map()
 
         self.migrate_user_comments()
-        self.migrate_user_submissions()
+        self.migrate_user_survey_submissions()
         self.migrate_user_poll_submissions()
-
-    def get_mapping_from_title(self, klass, title):
-        return klass.objects.get(title=title)
-
-    def get_mapping_from_path(self, klass, path, title):
-        section_index_pages = SectionIndexPage.objects.all()
-        survey_index_pages = SurveyIndexPage.objects.all()
-        all_parents = list(section_index_pages) + list(survey_index_pages)
-
-        possible_paths = [f'{parent.path}{path}' for parent in all_parents]
-
-        return klass.objects.filter(title=title, path__in=possible_paths).first()
-
-    def get_mapped_object(self, klass, title, path):
-        try:
-            obj = self.get_mapping_from_title(klass, title)
-        except MultipleObjectsReturned:
-            obj = self.get_mapping_from_path(Survey, path, title)
-
-        if not obj:
-            self.stdout.write(self.style.ERROR(f'{klass}: {title} -> No mapping found'))
-
-        return obj
-
-    def populate_surveys_map(self):
-        sql = f'select * from surveys_molosurveypage msp inner join wagtailcore_page wp on msp.page_ptr_id = wp.id'
-        cur = self.db_query(sql)
-
-        for row in self.with_progress(sql, cur, 'Populating Surveys Map'):
-            survey = self.get_mapped_object(Survey, row['title'], row['path'])
-
-            if survey:
-                self.surveys_map.update({
-                    row['id']: survey
-                })
-            else:
-                self.stdout.write(self.style.ERROR(f'Found no match for ({row["id"]}) - {row["title"]}'))
-
-        cur.close()
-
-    def populate_users_map(self):
-        sql = 'select * from auth_user'
-        cur = self.db_query(sql)
-
-        for row in self.with_progress(sql, cur, 'Populating Users map'):
-            self.users_map.update({
-                row["id"]: get_user_model().objects.get(username=row['username'])
-            })
-
-        cur.close()
-
-    def populate_articles_map(self):
-        sql = f'select * from core_articlepage cap inner join wagtailcore_page wp on cap.page_ptr_id = wp.id'
-        cur = self.db_query(sql)
-
-        for row in self.with_progress(sql, cur, 'Populating Articles Map'):
-            partial_path = row['path'][12:]
-            section_index_pages = SectionIndexPage.objects.all()
-
-            for sip in section_index_pages:
-                new_path = f'{sip.path}{partial_path}'
-
-                article = Article.objects.filter(path=new_path).first()
-                if article:
-                    self.articles_map.update({
-                        str(row['id']): article
-                    })
 
     def populate_content_type_map(self):
         sql = f'select * from django_content_type'
@@ -250,10 +175,8 @@ class Command(BaseCommand):
             user_data = dict(row)
             user_data.update({'has_filled_registration_survey': True})
 
-            get_user_model().objects.update_or_create(username=row['username'], defaults=user_data)
-            user = get_user_model().objects.filter(username=row['username']).first()
-            if not user:
-                user = get_user_model().objects.create(has_filled_registration_survey=True, **row)
+            user, __ = get_user_model().objects.update_or_create(username=row['username'], defaults=user_data)
+            V1ToV2ObjectMap.create_map(user, v1_user_id)
 
             user_groups_sql = f'select * from auth_user_groups aug ' \
                               f'inner join auth_group ag on aug.group_id = ag.id ' \
@@ -275,7 +198,8 @@ class Command(BaseCommand):
         cur = self.db_query(sql)
 
         for row in cur:
-            Group.objects.get_or_create(name=row['name'])
+            group, __ = Group.objects.get_or_create(name=row['name'])
+            V1ToV2ObjectMap.create_map(group, row['id'])
 
         self.stdout.write(self.style.SUCCESS('Completed User Groups Migration'))
 
@@ -290,17 +214,14 @@ class Command(BaseCommand):
         cur = self.db_query(sql)
 
         for row in self.with_progress(sql, cur, 'User comments migration in progress...'):
-            row.pop('id')
+            comment_id = row.pop('id')
             content_type = self.content_type_map[row['model']]
 
             if not content_type:
                 self.stdout.write(self.style.ERROR(f'Content Type for {row["model"]} not found.'))
                 continue
 
-            try:
-                new_article = self.articles_map[row["object_pk"]]
-            except KeyError:
-                new_article
+            new_article = V1ToV2ObjectMap.get_v2_obj(Article, row['object_pk'])
 
             max_thread_id_comment = XtdComment.objects.filter(
                 content_type_id=content_type.id, object_pk=new_article.pk).order_by('-thread_id').first()
@@ -309,20 +230,26 @@ class Command(BaseCommand):
             if max_thread_id_comment:
                 max_thread_id = max_thread_id_comment.id
 
-            XtdComment.objects.create(
-                content_type_id=content_type.id, object_pk=new_article.pk, user_name=row['user_name'],
-                user_email=row['user_email'], submit_date=row['submit_date'],
-                comment=row['comment'], is_public=True, is_removed=False, thread_id=max_thread_id + 1,
-                user=self.users_map[row['user_id']], order=1, followup=0, nested_count=0, site_id=1)
+            migrated_comment = V1ToV2ObjectMap.get_v2_obj(XtdComment, comment_id)
 
-    def migrate_user_submissions(self):
+            if not migrated_comment:
+                comment = XtdComment.objects.create(
+                    content_type_id=content_type.id, object_pk=new_article.pk, user_name=row['user_name'],
+                    user_email=row['user_email'], submit_date=row['submit_date'],
+                    comment=row['comment'], is_public=True, is_removed=False, thread_id=max_thread_id + 1,
+                    user=V1ToV2ObjectMap.get_v2_obj(get_user_model(), row['user_id']), order=1, followup=0, nested_count=0,
+                    site_id=1)
+                V1ToV2ObjectMap.create_map(comment, comment_id)
+
+    def migrate_user_survey_submissions(self):
         sql = 'select * from surveys_molosurveysubmission mss ' \
               'inner join surveys_molosurveypage msp on mss.page_id = msp.page_ptr_id'
         cur = self.db_query(sql)
 
         for row in self.with_progress(sql, cur, 'User Survey migration in progress...'):
             try:
-                new_survey = self.surveys_map[row['page_id']]
+                new_survey = V1ToV2ObjectMap.get_v2_obj(Survey, row['page_id'])
+                new_survey_page = Page.objects.get(pk=new_survey.id)
             except KeyError:
                 self.stdout.write(self.style.ERROR(f'Skipping Page: {row["page_id"]}'))
                 continue
@@ -336,28 +263,32 @@ class Command(BaseCommand):
                     new_key: value
                 })
 
-            UserSubmission.objects.create(
-                form_data=json.dumps(altered_form_data, cls=DjangoJSONEncoder),
-                page=new_survey,
-                user=self.users_map[row['user_id']] if row['user_id'] else None,
-            )
+            migrated_submission = V1ToV2ObjectMap.get_v2_obj(UserSubmission, row['id'], extra='survey')
+
+            if not migrated_submission:
+                submission = UserSubmission.objects.create(
+                    form_data=json.dumps(altered_form_data, cls=DjangoJSONEncoder),
+                    page=new_survey_page,
+                    user=V1ToV2ObjectMap.get_v2_obj(get_user_model(), row['user_id']) if row['user_id'] else None,
+                )
+                V1ToV2ObjectMap.create_map(submission, row['id'], extra='survey')
 
     def migrate_user_poll_submissions(self):
-        sql = 'select  pcv.user_id, pcv.question_id, wcp_question.title, wcp_question.path \
+        sql = 'select  pcv.user_id, pcv.question_id, wcp_question.title, wcp_question.id, wcp_question.path \
                 from polls_choice pc \
                     inner join wagtailcore_page wcp on pc.page_ptr_id = wcp.id \
                 inner join polls_choice_choice_votes pccv on pc.page_ptr_id = pccv.choice_id \
                 inner join polls_choicevote pcv on pccv.choicevote_id = pcv.id \
                 right outer join wagtailcore_page wcp_question on pcv.question_id = wcp_question.id \
                 where pcv.question_id is not null \
-                group by pcv.user_id, pcv.question_id, wcp_question.title,  wcp_question.path'
+                group by pcv.user_id, pcv.question_id, wcp_question.title, wcp_question.id,  wcp_question.path'
         cur = self.db_query(sql)
 
         for unique_submission in self.with_progress(sql, cur, 'User poll submissions migration in progress'):
             question_id = unique_submission['question_id']
             user_id = unique_submission['user_id']
             title = unique_submission['title']
-            path = unique_submission['path']
+            page_id = unique_submission['id']
 
             user_submissions_sql = f'select wcp.id, wcp.title as answer_title, pccv.choicevote_id, pcv.*, wcp_question.title \
                                     from polls_choice pc \
@@ -369,7 +300,8 @@ class Command(BaseCommand):
                                    f'pcv.user_id={user_id}'
             cursor = self.db_query(user_submissions_sql)
 
-            v2_poll = self.get_mapped_object(Poll, title, path)
+            v2_poll = V1ToV2ObjectMap.get_v2_obj(Poll, page_id)
+            v2_poll_page = Page.objects.get(pk=v2_poll.page_ptr_id)
 
             answers = []
             for row in cursor:
@@ -380,8 +312,13 @@ class Command(BaseCommand):
                 form_title: answers
             }
 
-            UserSubmission.objects.create(
-                form_data=json.dumps(form_data, cls=DjangoJSONEncoder),
-                page=v2_poll,
-                user=self.users_map[row['user_id']] if row['user_id'] else None,
-            )
+            migrated_submission = V1ToV2ObjectMap.get_v2_obj(UserSubmission, row['id'], extra='poll')
+
+            if not migrated_submission:
+                submission = UserSubmission.objects.create(
+                    form_data=json.dumps(form_data, cls=DjangoJSONEncoder),
+                    page=v2_poll_page,
+                    user=V1ToV2ObjectMap.get_v2_obj(get_user_model(), row['user_id']) if row['user_id'] else None,
+                )
+                V1ToV2ObjectMap.create_map(submission, row['id'], extra='poll')
+
