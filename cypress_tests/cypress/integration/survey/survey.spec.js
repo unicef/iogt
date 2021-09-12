86ed2ae0a69a02bda301eebb322c1f94e6f3821c
saqlainrasheed <msaqi681@gmail.com>
2021-09-12 20:32:49 +0500

    updates the survey.spec.js

--- cypress_tests/cypress/integration/survey/survey.spec.js
+++ cypress_tests/cypress/integration/survey/survey.spec.js
@@ -1,12 +1,12 @@
 describe('Survey submission tests', () => {
   it('Submit a survey and get back', () => {
-    cy.visit('/en/surveys/survey/')
-    cy.url().should('include', '/en/surveys/survey/')
-    cy.get('input[name=q1]')
+    cy.visit('/en/surveys/survey-title/')
+    cy.url().should('include', '/en/surveys/survey-title/')
+    cy.get('input[name=textfield]')
         .type('A1')
         .should('have.value', 'A1')
     cy.get('button[type=submit]').click()
     cy.get('.cust-btn.cust-btn--dark.survey-page__btn').click()
-    cy.url().should('include', '/en/surveys/survey/')
+    cy.url().should('include', '/en/surveys/survey-title/')
   })
 })
