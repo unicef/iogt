2e9bd9cd6b7ed338b6c1b6aefbfb5b1ee68d792b
Ehmad Zubair <ezubair@gmail.com>
2021-09-13 18:11:54 +0500

    Fixes #531

--- comments/templatetags/comment_tags.py
+++ comments/templatetags/comment_tags.py
@@ -8,3 +8,8 @@ def user_can_report(comment, user):
     if user.is_anonymous:
         return False
     return (comment.user != user) and not comment.flags.filter(user=user).exists()
+
+@register.simple_tag
+def filter_removed_comments(comments):
+    return [comment for comment in comments if not comment['comment'].is_removed]
+
