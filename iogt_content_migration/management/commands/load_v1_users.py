c26fef5f62143bff68297235fba3b9fc6d764b29
Ehmad Zubair <ezubair@gmail.com>
2021-09-20 16:53:12 +0500

    :construction: Completed comment reporting and ip_addresses migration

--- iogt_content_migration/management/commands/load_v1_users.py
+++ iogt_content_migration/management/commands/load_v1_users.py
@@ -265,7 +265,7 @@ class Command(BaseCommand):
                     user_email=row['user_email'], submit_date=row['submit_date'],
                     comment=row['comment'], is_public=True, is_removed=False, thread_id=max_thread_id + 1,
                     user=V1ToV2ObjectMap.get_v2_obj(get_user_model(), row['user_id']), order=1, followup=0,
-                    nested_count=0,
+                    nested_count=0, ip_address=row['ip_address'],
                     site_id=1)
                 V1ToV2ObjectMap.create_map(comment, comment_id)
 
