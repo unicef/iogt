4e562e1d26d0b455a7fef9011315d69c8295d0b4
Ehmad Zubair <ezubair@gmail.com>
2021-10-26 14:20:13 +0500

    Fixed username

--- comments/forms.py
+++ comments/forms.py
@@ -12,15 +12,16 @@ class CommentForm(BaseCommentForm):
         self.fields['followup'].initial = False
         self.fields['followup'].widget = forms.HiddenInput()
 
-        self.fields['post_anonymously'] = forms.BooleanField(label='Post Anonymnously?', required=False)
+        self.fields['post_anonymously'] = forms.BooleanField(
+            label='Don\'t display my username next to my comment', required=False)
 
     def get_comment_create_data(self, site_id=None):
         data = super().get_comment_create_data(site_id=site_id)
+
         if self.cleaned_data['post_anonymously']:
             data['user_email'] = ''
             data['user_name'] = 'anonymous'
         return data
-
 
 
 class AdminCommentForm(CommentForm):
