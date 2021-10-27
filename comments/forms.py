d195f9a40eb2c405787c27bfa789f805ca153e74
Ehmad Zubair <ezubair@gmail.com>
2021-10-27 13:24:21 +0500

    WIP with canned responses

--- comments/forms.py
+++ comments/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django_comments_xtd.forms import XtdCommentForm as BaseCommentForm
 
+from comments.models import CannedResponse
+
 
 class CommentForm(BaseCommentForm):
 
@@ -8,6 +10,13 @@ class CommentForm(BaseCommentForm):
         super(CommentForm, self).__init__(*args, **kwargs)
         self.fields['email'].initial = 'noreply@example.com'
         self.fields['email'].widget = forms.HiddenInput()
+
+        canned_responses_choices = [(None, '---')]
+
+        canned_responses_choices += ([(canned_response.text, canned_response.text) for canned_response in
+                                    CannedResponse.objects.all()])
+
+        self.fields['canned_responses'] = forms.ChoiceField(choices=canned_responses_choices, required=False)
 
         self.fields['followup'].initial = False
         self.fields['followup'].widget = forms.HiddenInput()
@@ -17,6 +26,9 @@ class CommentForm(BaseCommentForm):
 
     def get_comment_create_data(self, site_id=None):
         data = super().get_comment_create_data(site_id=site_id)
+
+        if self.cleaned_data['canned_responses']:
+            data['comment'] = f'{data["comment"]} {self.cleaned_data["canned_responses"]}'
 
         if self.cleaned_data['post_anonymously']:
             data['user_email'] = ''
