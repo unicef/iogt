130320cec8f196b20691c4849e61cf0ce6a83db7
mbilalhameed <mbilal.hameed@outlook.com>
2021-11-01 18:47:40 +0500

    resolve conflict in wagtail_hooks

--- home/wagtail_hooks.py
+++ home/wagtail_hooks.py
@@ -94,6 +94,11 @@ def limit_page_chooser(pages, request):
     return pages
 
 
+@hooks.register('insert_global_admin_css', order=100)
+def global_admin_css():
+    return format_html('<link rel="stylesheet" href="{}">', static('css/global/admin.css'))
+
+
 @hooks.register('insert_global_admin_js', order=100)
 def global_admin_js():
     return format_html(
