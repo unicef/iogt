660943dc3ee30ceed5e02556e736cdcf0e194952
Sher Ali <sheralim012@gmail.com>
2021-11-09 18:59:13 +0500

    fixes promote tab slug style

--- home/static/js/global/admin.js
+++ home/static/js/global/admin.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $("li.required.slug_field p.help").text(
+    $('#id_handle').parent().siblings('p[class=help]').text(
         'The handle must be written in the format "[language_code]_menu_live", e.g. "en_menu_live", for the menu to ' +
         'be live on the website. You can use other handles, e.g. "en_oldmenu", to store other draft menus without ' +
         'them getting displayed.'
