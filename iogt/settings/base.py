c33db6baf5229704b2ba425e54e61fab3972e6c2
Sher Ali <sheralim012@gmail.com>
2021-11-24 11:52:16 +0500

    changes priority for PO files to load into DB

--- iogt/settings/base.py
+++ iogt/settings/base.py
@@ -369,10 +369,10 @@ EXTRA_LANG_INFO = {
 django.conf.locale.LANG_INFO.update(EXTRA_LANG_INFO)
 
 LOCALE_PATHS = [
-    os.path.join(BASE_DIR, "locale"),
     os.path.dirname(django.conf.locale.__file__),
     os.path.join(os.path.dirname(allauth.__file__), 'locale'),
     os.path.join(os.path.dirname(auth.__file__), 'locale'),
+    os.path.join(BASE_DIR, "locale"),
 ]
 
 TRANSLATIONS_BASE_DIR = BASE_DIR
