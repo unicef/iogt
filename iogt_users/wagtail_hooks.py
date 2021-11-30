22aca2ebfb1336ebd89052dd81b0ee632ba325f4
Sher Ali <sheralim012@gmail.com>
2021-11-30 14:22:00 +0500

    optimizes user export query

--- iogt_users/wagtail_hooks.py
+++ iogt_users/wagtail_hooks.py
@@ -1,10 +1,14 @@
+from functools import cached_property
+
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.db.models import Prefetch
 from django.utils import timezone
 from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register
 
 from home.models import SiteSettings
 from iogt_users.filters import GroupsFilter
+from questionnaires.models import UserSubmission
 
 
 class UsersExportAdmin(ModelAdmin):
@@ -22,17 +26,41 @@ class UsersExportAdmin(ModelAdmin):
     menu_order = 601
 
     def registration_survey_response(self, obj):
+        user_submissions = obj.user_submissions
+
+        return user_submissions[0].form_data if user_submissions else ''
+
+    @cached_property
+    def get_registration_survey_ids(self):
+        ids = []
         registration_survey = SiteSettings.get_for_default_site().registration_survey
-        user_submission = None
         if registration_survey:
             ids = registration_survey.get_translations(inclusive=True).values_list('id', flat=True)
-            user_submission = obj.usersubmission_set.filter(page__pk__in=ids).order_by('submit_time').first()
 
-        return user_submission.form_data if user_submission else ''
+        return ids
 
     @property
     def export_filename(self):
         return f'users_{timezone.now().strftime(settings.EXPORT_FILENAME_TIMESTAMP_FORMAT)}'
+
+    def get_queryset(self, request):
+        """
+        Returns a QuerySet of all model instances that can be edited by the
+        admin site.
+        """
+        qs = self.model._default_manager.get_queryset()
+        ordering = self.get_ordering(request)
+        if ordering:
+            qs = qs.order_by(*ordering)
+
+        ids = self.get_registration_survey_ids
+        return qs.prefetch_related(
+            Prefetch(
+                'usersubmission_set',
+                queryset=UserSubmission.objects.filter(page__pk__in=ids),
+                to_attr='user_submissions'
+            )
+        )
 
 
 modeladmin_register(UsersExportAdmin)
