5e266c5d7b1c8a4f035cadde5b337a77a87323eb
Ehmad Zubair <ezubair@gmail.com>
2021-12-09 14:41:53 +0500

    Completed test case for image download

--- messaging/tests/webhook_tests.py
+++ messaging/tests/webhook_tests.py
@@ -7,7 +7,7 @@ from rest_framework.test import APITestCase
 
 from home.models import User
 from messaging.factories import ThreadFactory
-from messaging.models import Message
+from messaging.models import Message, Attachment
 
 
 class RapidProWebhookTest(APITestCase):
@@ -61,7 +61,7 @@ class RapidProWebhookTest(APITestCase):
         self.assertEqual(Message.objects.count(), 1)
         self.assertEqual(message.text, 'Some message with the first part of text.'
                                        ' First part ends at the exclamation mark!The second part starts here')
-        self.assertEqual(len(message.quick_replies.all()), 3)
+        self.assertEqual(len(message.quick_replies), 3)
 
     @override_settings(
         RAPIDPRO_BOT_USER_USERNAME='rb1',
@@ -141,3 +141,53 @@ class RapidProWebhookTest(APITestCase):
         self.assertEqual(message.attachments.count(), 1)
         self.assertEqual(attachment.external_link, 'http://www.internet.com/12345678910.jpg')
         self.assertEqual(len(message.quick_replies), 3)
+
+    @override_settings(
+        RAPIDPRO_BOT_USER_USERNAME='rb1',
+        RAPIDPRO_BOT_USER_PASSWORD='rapidpassword1',
+        RAPIDPRO_BOT_USER_ID=1
+    )
+    def test_single_download(self):
+        thread = ThreadFactory()
+
+        rapidpro_data_list = [
+            {
+                "id": "1",
+                "text": "Some message with attachment \nhttps://via.placeholder.com/200",
+                "to": str(thread.uuid),
+                "from": "abcd",
+                "channel": "bd3577c6-65b1-4bb7-9611-306c11b1dcc5",
+                "quick_replies": [
+                    "Baby (0 to 23 months old)", "Young child (2 to 9 years)", "Teenager (10 to 17 years)"
+                ]
+            },
+            {
+                "id": "2",
+                "text": "Another message with the same attachment \nhttps://via.placeholder.com/200",
+                "to": str(thread.uuid),
+                "from": "abcd",
+                "channel": "bd3577c6-65b1-4bb7-9611-306c11b1dcc5",
+                "quick_replies": [
+                    "Baby (0 to 23 months old)", "Young child (2 to 9 years)", "Teenager (10 to 17 years)"
+                ]
+            },
+
+        ]
+
+        for data in rapidpro_data_list:
+            response = self.client.post(path=reverse('messaging:api:rapidpro_webhook'), data=data, format='json')
+
+        messages = thread.get_renderable_messages()
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(len(messages), 2)
+        self.assertEqual(messages[0].text, 'Another message with the same attachment ')
+        self.assertEqual(messages[1].text, 'Some message with attachment ')
+        self.assertEqual(Attachment.objects.count(), 1)
+
+        attachment = Attachment.objects.first()
+        self.assertEqual('https://via.placeholder.com/200', attachment.external_link)
+
+        self.assertIsNotNone(attachment.image)
+
