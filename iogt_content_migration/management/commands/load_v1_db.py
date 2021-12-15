30b904e4777849c327288b30c37d77b8ecb617a9
Sher Ali <sheralim012@gmail.com>
2021-12-15 19:32:31 +0500

    minor fix

--- iogt_content_migration/management/commands/load_v1_db.py
+++ iogt_content_migration/management/commands/load_v1_db.py
@@ -1872,7 +1872,7 @@ class Command(BaseCommand):
 
         survey = Survey(
             title='Registration Survey', live=True, allow_multiple_submissions=True,
-            allow_anonymous_submissions=False, submit_button_text='Submit')
+            allow_anonymous_submissions=False, submit_button_text='Register')
 
         self.survey_index_page.add_child(instance=survey)
 
@@ -1915,7 +1915,7 @@ class Command(BaseCommand):
             if submit_button_text and len(submit_button_text) > 40:
                 self.stdout.write(f"Truncated survey submit button text, title={translated_survey.title}")
 
-            translated_survey.submit_button_text = submit_button_text[:40] if submit_button_text else 'Submit'
+            translated_survey.submit_button_text = submit_button_text[:40] if submit_button_text else 'Register'
             translated_survey.save()
             if translated_survey:
                 for (admin_label, label_identifier) in [
