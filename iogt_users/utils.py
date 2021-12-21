6f75a569b53db07a9b5b140c03b8e6fb808bc2e2
Usman Ali <68820888+UsmanAli99@users.noreply.github.com>
2021-12-21 10:43:19 +0500

    Update utils.py

--- iogt_users/utils.py
+++ iogt_users/utils.py
@@ -5,3 +5,4 @@ def has_completed_registration_survey(user):
     # TODO: Check if survey exists when surveys go live. This method should always return
     #  True if there is no post-registration survey
     return user.has_filled_registration_survey
+-
