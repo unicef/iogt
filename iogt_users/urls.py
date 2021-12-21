ade1ddc53a8eee3f910e4907741b895d62a7e0ad
UsmanAli <usman.ali@cogentlabs.co>
2021-12-21 16:44:59 +0500

    Remove legacy registration survey

--- iogt_users/urls.py
+++ iogt_users/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from iogt_users.views import UserDetailEditView, UserDetailView, PostRegistrationSurveyView
+from iogt_users.views import UserDetailEditView, UserDetailView
 
 urlpatterns = [
     path('profile/', UserDetailView.as_view(), name='user_profile'),
-    path('profile/edit', UserDetailEditView.as_view(), name='user_profile_edit'),
-    path('post-registration-survey', PostRegistrationSurveyView.as_view(), name='post_registration_survey')
+    path('profile/edit', UserDetailEditView.as_view(), name='user_profile_edit')
 ]
