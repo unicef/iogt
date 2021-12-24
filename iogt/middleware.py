41c01a04ea15d74bfa4f52605eda21e9210812ce
Sher Ali <sheralim012@gmail.com>
2021-12-24 12:36:27 +0500

    uses django locale middleware as base of custom local middelware

--- iogt/middleware.py
+++ iogt/middleware.py
@@ -1,9 +1,7 @@
 from django.conf import settings
 from django.conf.urls.i18n import is_language_prefix_patterns_used
-from django.http import HttpResponseRedirect
-from django.urls import get_script_prefix, is_valid_path
+from django.middleware.locale import LocaleMiddleware as DjangoLocaleMiddleware
 from django.utils import translation
-from django.utils.cache import patch_vary_headers
 from django.utils.deprecation import MiddlewareMixin
 from django.utils.translation import get_language_from_path, check_for_language, get_supported_language_variant
 from django.utils.translation.trans_real import get_languages, parse_accept_lang_header, language_code_re
@@ -25,24 +23,8 @@ class CacheControlMiddleware:
         return response
 
 
-class LocaleMiddleware(MiddlewareMixin):
-    """
-    Parse a request and decide what translation object to install in the
-    current thread context. This allows pages to be dynamically translated to
-    the language the user desires (if the language is available).
-    """
-    response_redirect_class = HttpResponseRedirect
-
+class LocaleMiddleware(DjangoLocaleMiddleware):
     def _get_language_from_request(self, request, check_path=False):
-        """
-        Analyze the request to find what language the user wants the system to
-        show. Only languages listed in settings.LANGUAGES are taken into account.
-        If the user requests a sublanguage where we have a main language, we send
-        out the main language.
-
-        If check_path is True, the URL path prefix will be checked for a language
-        code, otherwise this is skipped for backwards compatibility.
-        """
         if check_path:
             lang_code = get_language_from_path(request.path_info)
             if lang_code is not None:
@@ -82,41 +64,6 @@ class LocaleMiddleware(MiddlewareMixin):
         language = self._get_language_from_request(request, check_path=i18n_patterns_used)
         translation.activate(language)
         request.LANGUAGE_CODE = translation.get_language()
-
-    def process_response(self, request, response):
-        language = translation.get_language()
-        language_from_path = translation.get_language_from_path(request.path_info)
-        urlconf = getattr(request, 'urlconf', settings.ROOT_URLCONF)
-        i18n_patterns_used, prefixed_default_language = is_language_prefix_patterns_used(urlconf)
-
-        if (response.status_code == 404 and not language_from_path and
-                i18n_patterns_used and prefixed_default_language):
-            # Maybe the language code is missing in the URL? Try adding the
-            # language prefix and redirecting to that URL.
-            language_path = '/%s%s' % (language, request.path_info)
-            path_valid = is_valid_path(language_path, urlconf)
-            path_needs_slash = (
-                not path_valid and (
-                    settings.APPEND_SLASH and not language_path.endswith('/') and
-                    is_valid_path('%s/' % language_path, urlconf)
-                )
-            )
-
-            if path_valid or path_needs_slash:
-                script_prefix = get_script_prefix()
-                # Insert language after the script prefix and before the
-                # rest of the URL
-                language_url = request.get_full_path(force_append_slash=path_needs_slash).replace(
-                    script_prefix,
-                    '%s%s/' % (script_prefix, language),
-                    1
-                )
-                return self.response_redirect_class(language_url)
-
-        if not (i18n_patterns_used and language_from_path):
-            patch_vary_headers(response, ('Accept-Language',))
-        response.setdefault('Content-Language', language)
-        return response
 
 
 class AdminLocaleMiddleware(MiddlewareMixin):
