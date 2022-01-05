df1ad98bc067b358068034ca2ff71d7865205bd3
Ehmad Zubair <ezubair@gmail.com>
2022-01-05 16:22:14 +0500

    :wrench: Update create_default_site command

--- home/management/commands/create_default_site.py
+++ home/management/commands/create_default_site.py
@@ -8,9 +8,17 @@ User = get_user_model()
 
 class Command(BaseCommand):
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--domain',
+            help='The domain on which the website is deployed'
+        )
+
     def handle(self, *args, **options):
+        domain = options.get('domain') or 'localhost:8000'
+
         _, created = Site.objects.update_or_create(pk=settings.SITE_ID, defaults={
-            'domain': 'localhost:8000',
+            'domain': domain,
             'name': 'default'
         })
         self.stdout.write(self.style.SUCCESS(f'Site object {"created" if created else "updated"}'))
