fc49e3ffc09084616c3b1470246d4a7058f40906
Sher Ali <sheralim012@gmail.com>
2022-01-07 14:06:26 +0500

    adds hint for stop transltation

--- home/static/js/global/admin.js
+++ home/static/js/global/admin.js
@@ -4,4 +4,10 @@ $(document).ready(function () {
         'be live on the website. You can use other handles, e.g. "en_oldmenu", to store other draft menus without ' +
         'them getting displayed.'
     ).css({'color': 'red'});
+
+    $('#tab-content:contains("Download PO file and input translations offline")')
+        .find('>:first-child')
+        .prepend('<p style="margin-left: 50px; color: red; font-weight: bold;">' +
+            'Use the "Stop translation" option if you want to translate additional fields' +
+            '</p>')
 });
