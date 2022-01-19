314d86058fad4fd0c40dff190fde7048f29b1b5b
Sher Ali <sheralim012@gmail.com>
2022-01-19 16:32:22 +0500

    create/update wagtail site object in mgmt cmd

--- home/management/commands/create_default_site.py
+++ home/management/commands/create_default_site.py
@@ -1,7 +1,10 @@
 from django.conf import settings
 from django.contrib.auth import get_user_model
-from django.contrib.sites.models import Site
+from django.contrib.sites.models import Site as DjangoSite
 from django.core.management.base import BaseCommand
+from wagtail.core.models import Site as WagtailSite
+
+from home.models import HomePage
 
 User = get_user_model()
 
@@ -17,8 +20,14 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         domain = options.get('domain') or 'localhost:8000'
 
-        _, created = Site.objects.update_or_create(pk=settings.SITE_ID, defaults={
+        __, created = DjangoSite.objects.update_or_create(pk=settings.SITE_ID, defaults={
             'domain': domain,
             'name': 'default'
         })
-        self.stdout.write(self.style.SUCCESS(f'Site object {"created" if created else "updated"}'))
+        self.stdout.write(self.style.SUCCESS(f'Django Site object {"created" if created else "updated"}'))
+        __, created = WagtailSite.objects.update_or_create(is_default_site=True, defaults={
+            'hostname': domain,
+            'site_name': 'default',
+            'root_page': HomePage.objects.all().order_by('id').first()
+        })
+        self.stdout.write(self.style.SUCCESS(f'Wagtail Site object {"created" if created else "updated"}'))
