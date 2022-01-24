04c89ce4412836e88fea008d1494d0a418553dbc
Ed Moss <ed.moss@eemengineering.com>
2022-01-24 15:51:15 -0300

    Incorporating new translation management function

--- common/translation_utils/make_testing_po.py
+++ common/translation_utils/make_testing_po.py
@@ -3,75 +3,77 @@ import csv
 import copy
 import os
 
-po_metadata_template = {
-    "Report-Msgid-Bugs-To": "",
-    "POT-Creation-Date": "2021-07-31 09:38+0000",
-    "PO-Revision-Date": "2021-07-31 09:38+0000",
-    "Last-Translator": "FULL NAME <EMAIL@ADDRESS>",
-    "Language-Team": "LANGUAGE <LL@li.org>",
-    "Language": "",
-    "MIME-Version": "1.0",
-    "Content-Type": "text/plain; charset=UTF-8",
-    "Content-Transfer-Encoding": "8bit",
-    "Project-Id-Version": "PACKAGE VERSION",
-    "Plural-Forms": "nplurals=2; plural=(n > 1);",
-}
+def make_testing_po_run():
+
+    po_metadata_template = {
+        "Report-Msgid-Bugs-To": "",
+        "POT-Creation-Date": "2021-07-31 09:38+0000",
+        "PO-Revision-Date": "2021-07-31 09:38+0000",
+        "Last-Translator": "FULL NAME <EMAIL@ADDRESS>",
+        "Language-Team": "LANGUAGE <LL@li.org>",
+        "Language": "",
+        "MIME-Version": "1.0",
+        "Content-Type": "text/plain; charset=UTF-8",
+        "Content-Transfer-Encoding": "8bit",
+        "Project-Id-Version": "PACKAGE VERSION",
+        "Plural-Forms": "nplurals=2; plural=(n > 1);",
+    }
 
 
-sheet = open("translation_status.csv", newline='')
-reader = csv.reader(sheet)
+    sheet = open("common/translation_utils/translation_status.csv", newline='')
+    reader = csv.reader(sheet)
 
-sheet2 = open("translations.csv", newline='')
-data = list(csv.reader(sheet2))
-all_phrases = {r[3] : r for r in data}
-processed_phrases = set()
+    sheet2 = open("common/translation_utils/translations.csv", newline='')
+    data = list(csv.reader(sheet2))
+    all_phrases = {r[3] : r for r in data}
+    processed_phrases = set()
 
-entries = []
-entriesjs = []
-for i, row in enumerate(reader):
-    if i == 0:
-        po = polib.POFile()
-        po.metadata = copy.deepcopy(po_metadata_template)
-        po.metadata["Language"] = "Testing"
-        pojs = polib.POFile()
-        pojs.metadata = copy.deepcopy(po_metadata_template)
-        pojs.metadata["Language"] = "Testing"
-        continue
-    plural = False
-    phrase = row[0]
-    if phrase in processed_phrases:
-        continue
-    processed_phrases.add(phrase)
-    if phrase in all_phrases and all_phrases[phrase][0] == 'pl':
-        plural = True
-    tag = row[4].replace(' ', '-')
-    trans = f"([{tag}]-{phrase})"
+    entries = []
+    entriesjs = []
+    for i, row in enumerate(reader):
+        if i == 0:
+            po = polib.POFile()
+            po.metadata = copy.deepcopy(po_metadata_template)
+            po.metadata["Language"] = "Testing"
+            pojs = polib.POFile()
+            pojs.metadata = copy.deepcopy(po_metadata_template)
+            pojs.metadata["Language"] = "Testing"
+            continue
+        plural = False
+        phrase = row[0]
+        if phrase in processed_phrases:
+            continue
+        processed_phrases.add(phrase)
+        if phrase in all_phrases and all_phrases[phrase][0] == 'pl':
+            plural = True
+        tag = row[4].replace(' ', '-')
+        trans = f"([{tag}]-{phrase})"
 
-    if plural:
-        entry = entries[-1]
-        new_entry = polib.POEntry(
-            msgid=entry.msgid,
-            msgid_plural=phrase,
-            msgstr_plural={0: entry.msgstr, 1: trans}
-        )
-        entries[-1] = new_entry
-    else:
-        entry = polib.POEntry(
-            msgid=phrase,
-            msgstr=trans,
-        )
-        if row[3] == 'js':
-            entriesjs.append(entry)
+        if plural:
+            entry = entries[-1]
+            new_entry = polib.POEntry(
+                msgid=entry.msgid,
+                msgid_plural=phrase,
+                msgstr_plural={0: entry.msgstr, 1: trans}
+            )
+            entries[-1] = new_entry
         else:
-            entries.append(entry)
+            entry = polib.POEntry(
+                msgid=phrase,
+                msgstr=trans,
+            )
+            if row[3] == 'js':
+                entriesjs.append(entry)
+            else:
+                entries.append(entry)
 
-for entry in entries:
-    po.append(entry)
-for entry in entriesjs:
-    pojs.append(entry)
+    for entry in entries:
+        po.append(entry)
+    for entry in entriesjs:
+        pojs.append(entry)
 
-sheet.close()
+    sheet.close()
 
-os.makedirs("locale/xy/LC_MESSAGES/", exist_ok=True)
-po.save("locale/xy/LC_MESSAGES/django.po")
-pojs.save("locale/xy/LC_MESSAGES/djangojs.po")
+    os.makedirs("locale/xy/LC_MESSAGES/", exist_ok=True)
+    po.save("locale/xy/LC_MESSAGES/django.po")
+    pojs.save("locale/xy/LC_MESSAGES/djangojs.po")
---
+++ home/management/commands/translation_tracking.py
@@ -0,0 +1,29 @@
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+from common.translation_utils.update_status import *
+from common.translation_utils.make_testing_po import *
+from common.translation_utils.make_pos import *
+import polib
+import csv
+import copy
+import os
+
+#This management function runs a series of functions related to the translation process, for detailed information see common/translation_utils/README
+
+class Command(BaseCommand):
+    help = 'Updates translation translation tracking sheets and PO files'
+
+    def handle(self, *args, **options):
+        #Extracts message content from codebase and stores in locale/xy/LC_MESSAGES
+        call_command('makemessages', '-l' 'xy', '-d django')
+        call_command('makemessages', '-l' 'xy', '-d djangojs')
+
+        # Processes messages from locale/xy/LC_MESSAGES, looks through ‘translation_status.csv’ and ‘translations.csv’ 
+        # and creates an up to date log of translation which are present and their status
+        update_status_run()
+
+        # Uses the excel files to create the necessary PO files, stores them in locale. 
+        # Also creates a file ‘translatable_strings’ with translations that we need but don’t have
+        make_testing_po_run()
+        make_pos_run()
+        
