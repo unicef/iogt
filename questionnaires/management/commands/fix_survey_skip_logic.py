e34cc37bb71ac296b2e405865f8eafc549d8bcfc
Sher Ali <sheralim012@gmail.com>
2022-02-03 17:50:07 +0500

    adds mgmt cmd to fix surveys skip logic

---
+++ questionnaires/management/commands/fix_survey_skip_logic.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+
+from questionnaires.models import SurveyFormField
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        survey_form_fields = SurveyFormField.objects.all()
+        for survey_form_field in survey_form_fields:
+            if survey_form_field.skip_logic:
+                skip_logics = survey_form_field.skip_logic.stream_data
+                for skip_logic in skip_logics:
+                    skip_logic.get('value', {}).pop('survey')
+
+        SurveyFormField.objects.bulk_update(survey_form_fields, fields=['skip_logic'], batch_size=1000)
+
+        self.stdout.write(self.style.SUCCESS('Fixed surveys skip logic.'))
