1ad136ca4a66065777ddfaa682bfd7051f95a1b5
Sher Ali <sheralim012@gmail.com>
2022-02-17 18:43:28 +0500

    refine logic for sandbox mgmt cmd

--- home/management/commands/create_sandbox_section.py
+++ home/management/commands/create_sandbox_section.py
@@ -7,10 +7,11 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         home_page = HomePage.objects.get(slug='home')
-        if not Section.objects.filter(slug='sandbox').exists():
+        sandbox_section = home_page.get_children().filter(slug='sandbox').first()
+        if sandbox_section:
+            self.stdout.write(self.style.SUCCESS('Sandbox Section already exists.'))
+        else:
             section = Section(title='Sandbox', live=False)
             home_page.add_child(instance=section)
             self.stdout.write(self.style.SUCCESS('Sandbox Section added.'))
-        else:
-            self.stdout.write(self.style.SUCCESS('Sandbox Section already exists.'))
 
