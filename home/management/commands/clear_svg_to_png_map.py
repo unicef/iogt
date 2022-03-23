30294f6544d950ed48fe827d756b5268738de118
Sher Ali <sheralim012@gmail.com>
2022-03-23 15:59:46 +0500

    delete files from system with deleting from db

--- home/management/commands/clear_svg_to_png_map.py
+++ home/management/commands/clear_svg_to_png_map.py
@@ -5,5 +5,8 @@ from home.models import SVGToPNGMap
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        SVGToPNGMap.objects.all().delete()
+        for map in SVGToPNGMap.objects.all():
+            map.delete()
+            map.png_image_file.delete()
+
         self.stdout.write(self.style.SUCCESS('SVG to PNG map cleared.'))
