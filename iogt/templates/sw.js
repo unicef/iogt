4d355f34632891d7f28a30aceb8a483fc66b361b
Sher Ali <sheralim012@gmail.com>
2022-04-11 13:37:05 +0500

    minor changes to caching

--- iogt/templates/sw.js
+++ iogt/templates/sw.js
@@ -1,33 +1,36 @@
 importScripts('../static/third_party/workbox/workbox-v6.1.5/workbox-sw.js');
 
-
 // Below we are using a custom dimension to track online vs. offline interactions. So make
 // sure to create a custom dimension on GA
-
 workbox.googleAnalytics.initialize({
-  parameterOverrides: {
-    cd1: 'offline',
-  },
+    parameterOverrides: {
+        cd1: 'offline',
+    },
 });
 
 const precacheController = new workbox.precaching.PrecacheController();
 
-self.addEventListener('install', event => {
-    const resp = fetch(`${location.protocol}//${location.host}/sitemap/`)
+self.addEventListener('install', (event) => {
+    const resp = fetch('/sitemap/')
         .then(response => response.json())
         .then(urls => {
             precacheController.addToCacheList(urls.map(url => ({
                 url,
                 revision: null,
             })));
-            workbox.precaching.precacheAndRoute(urls.map(url => ({
-                url,
-                revision: null,
-            })));
-        })
-        .then(() => {
+        }).then(() => {
             // Passing in event is required in Workbox v6+
-            precacheController.install(event);
-        });
+            event.waitUntil(precacheController.install(event));
+        })
     event.waitUntil(resp);
+});
+
+self.addEventListener('activate', (event) => {
+    // Passing in event is required in Workbox v6+
+    event.waitUntil(precacheController.activate(event));
+});
+
+self.addEventListener('fetch', (event) => {
+    const cacheKey = precacheController.getCacheKeyForURL(event.request.url);
+    event.respondWith(caches.match(cacheKey));
 });
