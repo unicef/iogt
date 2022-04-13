acae647e74863fc25a34bc448989c8d5305b0e76
Sher Ali <sheralim012@gmail.com>
2022-04-14 01:03:47 +0500

    delete chat too

--- home/management/commands/clear_user_data.py
+++ home/management/commands/clear_user_data.py
@@ -2,6 +2,8 @@ from django.contrib.auth import get_user_model
 from django.core.management import BaseCommand
 from django_comments_xtd.models import XtdComment
 
+from messaging.models import Thread
+
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
@@ -17,4 +19,5 @@ class Command(BaseCommand):
         user_ids = options.get('exclude_user_ids') or []
         get_user_model().objects.exclude(pk__in=user_ids).delete()
         XtdComment.objects.all().delete()
+        Thread.objects.all().delete()
         self.stdout.write('User data cleared.')
