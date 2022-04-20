f2c127c43199fee990700fc2fa9e2596f28f7187
Sher Ali <sheralim012@gmail.com>
2022-04-20 14:10:48 +0500

    fixes rendering

--- questionnaires/wagtail_hooks.py
+++ questionnaires/wagtail_hooks.py
@@ -1,14 +1,17 @@
+import locale
 from django.urls import path
 from django.utils.html import format_html_join
 from django.templatetags.static import static
 
 from wagtail.core import hooks
+from wagtail.core.models import Locale
 
 from questionnaires.views import FormPagesListView
 
 
 @hooks.register('insert_editor_js', order=100)
 def editor_js():
+    language_code = Locale.get_active().language_code
     js_files = [
         'js/blocks/skiplogic.js',
         'js/blocks/skiplogic_stream.js',
@@ -16,6 +19,7 @@ def editor_js():
     js_includes = format_html_join('\n', '<script src="{0}"></script>',
         ((static(filename),) for filename in js_files)
     )
+    js_includes = f'{js_includes}\n<script src="/{language_code}/jsi18n/"></script>'
     return js_includes
 
 
