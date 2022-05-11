78c7fac07d5a98fbbcc73ca32d76dde9ce093378
edmoss94 <ed.moss@eemengineering.com>
2022-05-11 15:35:27 -0300

    more attempts to get docker working

--- selenium_tests/docker_tests/tests_internal.py
+++ selenium_tests/docker_tests/tests_internal.py
@@ -1,48 +1,29 @@
 import time
-import socket
-from django.test import override_settings, tag
+
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
-@override_settings(ALLOWED_HOSTS=['*'])
 class SeleniumTest(LiveServerTestCase):
-    """
-    Provides base test class which connects to the Docker
-    container running selenium.
-    """
-    host = '0.0.0.0'
-
     
     def setUp(self):
-        self.host = socket.gethostbyname(socket.gethostname())
-        self.selenium = webdriver.Remote(
-            command_executor='http://localhost:4444/wd/hub',
+        self.chrome = webdriver.Remote(
+            command_executor='http://selenium-hub:4444/wd/hub',
+            desired_capabilities=DesiredCapabilities.CHROME
+      )      
+
+        self.firefox = webdriver.Remote(
+            command_executor='http://selenium-hub:4444/wd/hub',
             desired_capabilities=DesiredCapabilities.FIREFOX
       )
-
-    fixtures = ['basic_fixture.json']
-
-    def login_page_load(self):
-        self.selenium.get('%s%s' % (self.live_server_url, '/accounts/login/'))
-        assert 'Log in' in self.selenium.title
-
-    def test_login(self): 
-        self.selenium.get('%s%s' % (self.live_server_url, '/accounts/login/'))       
-        username_input = self.selenium.find_element_by_name("login")
-        username_input.send_keys('tester')
-        time.sleep(1)
-        password_input = self.selenium.find_element_by_name("password")
-        password_input.send_keys('tester')
-        time.sleep(1)
-        submit = self.selenium.find_element_by_xpath("//button[@type='submit']")
-        submit.send_keys(Keys.RETURN)
-        time.sleep(1)
-
-    def login_result(self):
-        assert 'tester' in self.selenium.page_source
+       
+    def test_homepage(self):
+        browser = self.chrome
+        browser.get(self.live_server_url)
+        assert 'Home' in self.selenium.title
 
     def tearDown(self):
-        self.selenium.quit()  # quit vs close?
+        self.chrome.quit()
+        self.firefox.quit()  # quit vs close?
