68ec113b13b3575821bcbaf44f900cda3ce005ec
Sher Ali <sheralim012@gmail.com>
2022-07-06 14:42:01 +0500

    optimzes form data export

--- questionnaires/views.py
+++ questionnaires/views.py
@@ -1,3 +1,6 @@
+import csv
+
+from django.http import HttpResponse
 from wagtail.contrib.forms.views import SubmissionsListView, FormPagesListView as WagtailFormPagesListView
 
 
@@ -17,3 +20,45 @@ class FormPagesListView(WagtailFormPagesListView):
 class CustomSubmissionsListView(SubmissionsListView):
     def get_filename(self):
         return self.form_page.get_export_filename()
+
+    def get_queryset(self):
+        """ Return queryset of form submissions with filter and order_by applied """
+        submission_class = self.form_page.get_submission_class()
+        queryset = submission_class._default_manager.filter(page=self.form_page).select_related('page', 'user')
+
+        filtering = self.get_filtering()
+        if filtering and isinstance(filtering, dict):
+            queryset = queryset.filter(**filtering)
+
+        ordering = self.get_ordering()
+        if ordering:
+            if isinstance(ordering, str):
+                ordering = (ordering,)
+            queryset = queryset.order_by(*ordering)
+
+        return queryset
+
+    def write_csv_row(self, writer, row_dict):
+        processed_row = {}
+        for field, value in row_dict.items():
+            preprocess_function = self.get_preprocess_function(
+                field, value, self.FORMAT_CSV
+            )
+            processed_value = (
+                preprocess_function(value) if preprocess_function else value
+            )
+            processed_row[field] = processed_value
+        return writer.writerow(processed_row.values())
+
+    def write_csv_response(self, queryset):
+        response = HttpResponse(
+            content_type='text/csv',
+        )
+        response["Content-Disposition"] = 'attachment; filename="{}.csv"'.format(
+            self.get_filename()
+        )
+        writer = csv.writer(response)
+        writer.writerow({field: self.get_heading(queryset, field) for field in self.list_export})
+        for item in queryset:
+            self.write_csv_row(writer, self.to_row_dict(item))
+        return response
