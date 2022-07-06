0291424ab11a24ecc3467b735ef4b741f9122ea7
Sher Ali <sheralim012@gmail.com>
2022-07-06 16:59:58 +0500

    write all records at once

--- questionnaires/views.py
+++ questionnaires/views.py
@@ -22,23 +22,9 @@ class CustomSubmissionsListView(SubmissionsListView):
         return self.form_page.get_export_filename()
 
     def get_queryset(self):
-        """ Return queryset of form submissions with filter and order_by applied """
-        submission_class = self.form_page.get_submission_class()
-        queryset = submission_class._default_manager.filter(page=self.form_page).select_related('page', 'user')
+        return super().get_queryset().select_related('page', 'user')
 
-        filtering = self.get_filtering()
-        if filtering and isinstance(filtering, dict):
-            queryset = queryset.filter(**filtering)
-
-        ordering = self.get_ordering()
-        if ordering:
-            if isinstance(ordering, str):
-                ordering = (ordering,)
-            queryset = queryset.order_by(*ordering)
-
-        return queryset
-
-    def write_csv_row(self, writer, row_dict):
+    def process_csv_row(self, row_dict):
         processed_row = {}
         for field, value in row_dict.items():
             preprocess_function = self.get_preprocess_function(
@@ -48,7 +34,7 @@ class CustomSubmissionsListView(SubmissionsListView):
                 preprocess_function(value) if preprocess_function else value
             )
             processed_row[field] = processed_value
-        return writer.writerow(processed_row.values())
+        return processed_row.values()
 
     def write_csv_response(self, queryset):
         response = HttpResponse(
@@ -59,6 +45,8 @@ class CustomSubmissionsListView(SubmissionsListView):
         )
         writer = csv.writer(response)
         writer.writerow({field: self.get_heading(queryset, field) for field in self.list_export})
+        items = []
         for item in queryset:
-            self.write_csv_row(writer, self.to_row_dict(item))
+            items.append(self.process_csv_row(self.to_row_dict(item)))
+        writer.writerows(items)
         return response
