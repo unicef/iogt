4f92a2e0c0078f3cafaded7b26ab37bca123c1ca
Sher Ali <sheralim012@gmail.com>
2022-07-20 23:04:32 +0500

    fix sw registration

--- iogt/static/js/sw-init.js
+++ iogt/static/js/sw-init.js
@@ -1,10 +1,14 @@
-if ('serviceWorker' in navigator) {
-    await navigator.serviceWorker.register(serviceWorkerURL, {scope: '/'});
-    const isPushNotificationRegistered = getItem('isPushNotificationRegistered', false);
-    if (!isPushNotificationRegistered) {
-        console.log('Push notification not registered.')
-        registerPushNotification(registration);
-    } else {
-        console.log('Push notification already registered.')
+const registerSW = async () => {
+    if ('serviceWorker' in navigator) {
+        const registration = await navigator.serviceWorker.register(serviceWorkerURL, {scope: '/'});
+        const isPushNotificationRegistered = getItem('isPushNotificationRegistered', false);
+        if (!isPushNotificationRegistered) {
+            console.log('Push notification not registered.')
+            registerPushNotification(registration);
+        } else {
+            console.log('Push notification already registered.')
+        }
     }
-}
+};
+
+registerSW();
