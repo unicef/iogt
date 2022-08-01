c022c6a7ebe47cea426610ad7aee5c2254df1e8a
Sher Ali <sheralim012@gmail.com>
2022-08-01 12:02:45 +0500

    remove custom menu item handling

--- notifications/wagtail_hooks.py
+++ notifications/wagtail_hooks.py
@@ -17,13 +17,3 @@ class NotificationModelAdmin(ModelAdmin):
 
 
 modeladmin_register(NotificationModelAdmin)
-
-
-@hooks.register('construct_main_menu')
-def hide_notification_menu_item_from_unauthorized_users(request, menu_items):
-    user = request.user
-    if user.is_superuser:
-        return
-
-    if request.user.has_perms(['notification.add_notification', 'notification.view_notification']):
-        menu_items[:] = [item for item in menu_items if item.name != 'notification']
