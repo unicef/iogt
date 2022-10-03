dc9cad35d26c8d210165ab1a2febeaa9d4c34f2c
Alihassanc5 <Alihassan01264@gmail.com>
2022-10-03 15:44:30 +0500

    Bypass registeration survey for admin users

--- iogt_users/middlewares.py
+++ iogt_users/middlewares.py
@@ -17,7 +17,7 @@ class RegistrationSurveyRedirectMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        if not request.user.is_superuser:
+        if request.user.is_superuser:
             return self.get_response(request)
 
         request_path = request.path_info
