3289f49352c509003a26d8520ea6309e70e3ec2c
Alihassanc5 <Alihassan01264@gmail.com>
2022-10-26 18:18:04 +0500

    minor changes

--- home/signals.py
+++ home/signals.py
@@ -1,6 +1,7 @@
 from django.dispatch import receiver
 from wagtail.core.models import Site
 from django.db.models.signals import pre_save, pre_delete
+from wagtail.admin import messages
 
 
 @receiver(pre_save, sender=Site)
@@ -9,6 +10,6 @@ def save_site(instance, **kwargs):
         instance.is_default_site = True
 
 @receiver(pre_delete, sender=Site)
-def delete_site(instance, **kwargs):
+def delete_site(sender, instance, using, **kwargs):
     if Site.objects.exclude(pk=instance.pk).count() == 1:
         Site.objects.exclude(pk=instance.pk).update(is_default_site=True)
