2e02c61989ed682666a928b16888a6588cc78e62
edmoss94 <ed.moss@eemengineering.com>
2022-11-02 20:47:32 -0300

    Setting up pages for banner tests

--- selenium_tests/homepage/test_banners.py
+++ selenium_tests/homepage/test_banners.py
@@ -1,12 +1,19 @@
 from selenium_tests.base import BaseSeleniumTests
 from home.factories import (
-    FooterIndexPageFactory,
-    SectionFactory,
-    SectionIndexFactory,
+    BannerIndexPageFactory,
+    BannerFactory,
+    ArticleFactory
 )
 
 class BannerSeleniumTests(BaseSeleniumTests):
 
     def setUp(self):
         self.setup_blank_site()
+        self.banner_index = BannerIndexPageFactory(parent=self.site.root_page)
+
+    def test_banner_is_present(self):
+        self.article01 = ArticleFactory(parent = self.site.root_page, title = 'article01')
+        self.banner01 = BannerFactory(parent = self.banner_index, title = 'banner01')        
+
+        home_page = self.visit_page(self.home)
         
