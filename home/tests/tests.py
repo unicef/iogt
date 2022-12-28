fcf7f12b5a46109622beeb38c3db7f39ec970a11
Ali Hassan <alihassan01264@gmail.com>
2022-12-28 17:01:23 +0500

    adds unit test cases

--- home/tests/tests.py
+++ home/tests/tests.py
@@ -1,9 +1,11 @@
+from bs4 import BeautifulSoup
 from django.test import TestCase
 from django.http import HttpRequest
 from wagtail.core.models import Site
+from wagtail_localize.operations import TranslationCreator
 
 from home.wagtail_hooks import limit_page_chooser
-from home.factories import SectionFactory, ArticleFactory, HomePageFactory
+from home.factories import SectionFactory, ArticleFactory, HomePageFactory, LocaleFactory
 from wagtail_factories import SiteFactory
 
 
@@ -55,3 +57,29 @@ class LimitPageChooserHookTests(TestCase):
         pages_after = limit_page_chooser(pages_before, request)
 
         self.assertEqual(pages_after, pages_before)
+
+
+class LanguageSelectorTest(TestCase):
+    def setUp(self):
+        self.site = Site.objects.get(is_default_site=True)
+        self.home_page = self.site.root_page
+
+    def test_language_selector_drop_down_for_one_language(self):
+        response = self.client.get(self.home_page.url)
+        parsed_response = BeautifulSoup(response.content)
+        language_drop_down = parsed_response.find("div", {"class": "language_drop"})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNone(language_drop_down)
+
+    def test_language_selector_drop_down_for_multiple_languages(self):
+        bn_locale = LocaleFactory(language_code='bn')
+        bn_translation_creator = TranslationCreator(user=None, target_locales=[bn_locale])
+        bn_translation_creator.create_translations(self.home_page)
+
+        response = self.client.get(self.home_page.url)
+        parsed_response = BeautifulSoup(response.content)
+        language_drop_down = parsed_response.find("div", {"class": "language_drop"})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNotNone(language_drop_down)
