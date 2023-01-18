e4f6f6d2e741716663b540fe8e2b4bc257ecb827
Ali Hassan <alihassan01264@gmail.com>
2023-01-18 11:57:20 +0500

    Adds unit tests

--- home/tests/tests.py
+++ home/tests/tests.py
@@ -1,10 +1,14 @@
+from bs4 import BeautifulSoup
 from django.test import TestCase
 from django.http import HttpRequest
-from wagtail.core.models import Site
+from django.urls import reverse
+from wagtail.core.models import Site, Page
 
 from home.wagtail_hooks import limit_page_chooser
-from home.factories import SectionFactory, ArticleFactory, HomePageFactory
+from home.factories import SectionFactory, ArticleFactory, HomePageFactory, SectionIndexFactory
 from wagtail_factories import SiteFactory
+
+from iogt_users.factories import AdminUserFactory
 
 
 class LimitPageChooserHookTests(TestCase):
@@ -55,3 +59,35 @@ class LimitPageChooserHookTests(TestCase):
         pages_after = limit_page_chooser(pages_before, request)
 
         self.assertEqual(pages_after, pages_before)
+
+
+class PageViewTest(TestCase):
+    def setUp(self):
+        root_page = Page.objects.filter(depth=1).first()
+        self.home_page = HomePageFactory(parent=root_page)
+        SiteFactory(hostname='testserver', port=80, root_page=self.home_page)
+        user = AdminUserFactory()
+
+        self.section_index_page = SectionIndexFactory(parent=self.home_page)
+        self.client.force_login(user)
+
+    def test_view_live_button_on_parent_page_listing(self):
+        response = self.client.get(reverse('wagtailadmin_explore', args=[self.home_page.pk]))
+        parsed_response = BeautifulSoup(response.content)
+        view_live_button = parsed_response.find("tbody").find("ul", {"class": "actions"}).find("a", {"title": "View live"})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNone(view_live_button)
+
+    def test_view_live_buttons_on_edit_page(self):
+        response = self.client.get(reverse('wagtailadmin_pages:edit', args=[self.section_index_page.pk]))
+        parsed_response = BeautifulSoup(response.content)
+        header_live_button = parsed_response.find("header").find("a", {"title": "Visit the live page"})
+        footer_preview_button = parsed_response.find("footer").find("li", {"class": "preview"}).find("button")
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNone(header_live_button)
+        self.assertIsNone(footer_preview_button)
+
+
+
