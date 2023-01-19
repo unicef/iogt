089720657e874be52e7f85677b6c58f326e6e7e5
Ali Hassan <alihassan01264@gmail.com>
2023-01-19 15:38:31 +0500

    Refactor code

--- questionnaires/templatetags/questionnaires_tags.py
+++ questionnaires/templatetags/questionnaires_tags.py
@@ -40,7 +40,27 @@ def render_text_field(field):
 
 @register.inclusion_tag('questionnaires/tags/field_description.html')
 def field_description(field):
-    return {"field": field}
+    field_type = field.widget_type
+    description = ''
+
+    if not field.field.required or field.help_text or field_type == "radioselect" or field_type == "select" or field_type == "checkboxselectmultiple" \
+            or field_type == "selectmultiple" or field_type == "checkbox" or field_type == "date" or field_type == "datetime":
+        if not field.field.required:
+            description = 'Optional'
+        elif field.help_text:
+            description = field.help_text
+        elif field_type == "radioselect" or field_type == "select":
+            description = 'Select one'
+        elif field_type == "checkboxselectmultiple" or field_type == "selectmultiple":
+            description = 'Check all that apply'
+        elif field_type == "checkbox":
+            description = 'Check if applies'
+        elif field_type == "date":
+            description = 'Date must be in this (YYYY-MM-DD) format'
+        elif field_type == "datetime":
+            description = 'Datetime must be in this YYYY-MM-DDTHH:SS format'
+
+    return {'description': description}
 
 
 @register.inclusion_tag('questionnaires/tags/render_fields.html')
