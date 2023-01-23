82de52a30cb03718e4c0502fe4108b34b421fa3e
Ali Hassan <alihassan01264@gmail.com>
2023-01-23 16:39:03 +0500

    minor changes

--- home/image_formats.py
+++ home/image_formats.py
@@ -8,9 +8,9 @@ for image_format in image_formats:
     unregister_image_format(image_format.name)
 
 
-register_image_format(Format('fullwidth', _('Full width'), 'richtext-image full-width', 'width-700'))
-register_image_format(Format('left', _('Left-aligned'), 'richtext-image left', 'width-700'))
-register_image_format(Format('right', _('Right-aligned'), 'richtext-image right', 'width-700'))
+register_image_format(Format('fullwidth', _('Full width'), 'richtext-image full-width', 'width-750'))
+register_image_format(Format('left', _('Left-aligned'), 'richtext-image left', 'width-750'))
+register_image_format(Format('right', _('Right-aligned'), 'richtext-image right', 'width-750'))
 register_image_format(Format('original',
                              _('Original Size (Can use LARGE amounts of user data package, not recommended)'),
                              'richtext-image original', 'original'))
