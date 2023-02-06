62730f2c31995e91c3e8b843e9a0770a50f1ca22
Ali Hassan <62539376+Alihassanc5@users.noreply.github.com>
2023-02-06 18:35:47 +0500

    Bypass registration survey for admin users (#1411)
    Co-authored-by: Sher Ali <49383675+sheralim012@users.noreply.github.com>

--- iogt_users/tests.py
+++ iogt_users/tests.py
@@ -3,23 +3,22 @@ from rest_framework import status
 from wagtail.core.models import Site
 
 from home.factories import SiteSettingsFactory, HomePageFactory
-from iogt_users.factories import UserFactory
-from wagtail_factories import SiteFactory
+from iogt_users.factories import UserFactory, AdminUserFactory, GroupFactory
+from wagtail_factories import SiteFactory, PageFactory
 from questionnaires.factories import SurveyFactory
+from django.contrib.auth.models import Permission
 
 
 class PostRegistrationRedirectTests(TestCase):
     def setUp(self):
         self.user = UserFactory(has_filled_registration_survey=False)
-        self.admin_user = UserFactory()
 
-        Site.objects.all().delete()
-        self.site = SiteFactory(site_name='IoGT', port=8000, is_default_site=True)
-        self.home_page = HomePageFactory(parent=self.site.root_page)
+        root_page = PageFactory(parent=None)
+        self.home_page = HomePageFactory(parent=root_page)
+        self.site = SiteFactory(hostname='testserver', port=80, root_page=self.home_page)
 
         self.registration_survey = SurveyFactory(parent=self.home_page)
-        self.site_settings = SiteSettingsFactory.create(
-            registration_survey=self.registration_survey, site=self.site)
+        SiteSettingsFactory.create(registration_survey=self.registration_survey, site=self.site)
 
     def test_user_locked_out_without_filling_registration_survey_form(self):
         self.client.force_login(self.user)
@@ -29,4 +28,31 @@ class PostRegistrationRedirectTests(TestCase):
 
     def test_anonymous_user_can_browse_public_urls(self):
         response = self.client.get(self.home_page.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_superuser_should_not_complete_registration_survey(self):
+        admin_user = AdminUserFactory(has_filled_registration_survey=False)
+
+        self.client.force_login(admin_user)
+        response = self.client.get(self.home_page.url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_with_admin_access_should_not_complete_registration_survey(self):
+        admin_access_permission = Permission.objects.get(codename='access_admin')
+        group = GroupFactory()
+        group.permissions.add(admin_access_permission)
+
+        self.user.groups.add(group)
+        self.client.force_login(self.user)
+        response = self.client.get(self.home_page.url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_should_not_redirect_after_filling_registration_survey(self):
+        self.user.has_filled_registration_survey = True
+        self.user.save()
+        self.client.force_login(self.user)
+        response = self.client.get(self.home_page.url)
+
         self.assertEqual(response.status_code, status.HTTP_200_OK)
