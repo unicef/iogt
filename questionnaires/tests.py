3e304a9c87f3dc5e95dace8eb2fd83892086eb62
Ali Hassan <alihassan01264@gmail.com>
2023-02-08 15:42:41 +0500

    Update unit tests

--- questionnaires/tests.py
+++ questionnaires/tests.py
@@ -1,7 +1,6 @@
 import datetime
 import io
 import json
-import urllib.parse
 from datetime import timedelta
 
 import pytz
@@ -1039,14 +1038,17 @@ class PollTest(TestCase):
         poll_question = PollFormFieldFactory(page=self.poll, field_type='checkboxes', choices='c1|c2|c3')
 
         response = self.client.post(self.poll.url, {poll_question.clean_name: ('c1', 'c2')})
-        parsed_response = BeautifulSoup(response.content)
-        results = parsed_response.findAll('div', {'class': 'cust-check__title'})
+        results = response.context_data['results']
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(results), 3)
-        self.assertEqual(results[0].find('div', {'class': 'cust-check__title-left'}).text.strip(), 'c1\nYour answer')
-        self.assertEqual(" ".join(results[0].find('div', {'class': 'cust-check__title-right'}).text.split()), '100 %')
-        self.assertEqual(results[1].find('div', {'class': 'cust-check__title-left'}).text.strip(), 'c2\nYour answer')
-        self.assertEqual(" ".join(results[1].find('div', {'class': 'cust-check__title-right'}).text.split()), '100 %')
-        self.assertEqual(results[2].find('div', {'class': 'cust-check__title-left'}).text.strip(), 'c3')
-        self.assertEqual(" ".join(results[2].find('div', {'class': 'cust-check__title-right'}).text.split()), '0 %')
+        self.assertEqual(results[0][0], 'c1')
+        self.assertEqual(results[1][0], 'c2')
+        self.assertEqual(results[2][0], 'c3')
+        self.assertEqual(results[0][1], 100.0)
+        self.assertEqual(results[1][1], 100.0)
+        self.assertEqual(results[2][1], 0.0)
+        self.assertTrue(results[0][2])
+        self.assertTrue(results[1][2])
+        self.assertFalse(results[2][2])
+
