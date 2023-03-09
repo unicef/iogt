6f61a627859ac1aaf74854e2f4667210bb655956
Sher Ali <sheralim012@gmail.com>
2023-03-09 12:21:00 +0500

    removes v2 questionnaires apis

--- questionnaires/api/v2/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from questionnaires.api.v2.views import (
-    QuestionnaireSubmissionsAPIView,
-)
-
-urlpatterns = [
-    path('<int:pk>/submissions/', QuestionnaireSubmissionsAPIView.as_view(), name='v2_questionnaire_submissions'),
-]
