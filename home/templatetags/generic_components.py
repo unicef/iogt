13d090d912e8f1dfed71fd4718a928674eb10ce9
Ali Hassan <alihassan01264@gmail.com>
2023-03-16 15:16:36 +0500

    refactor code

--- home/templatetags/generic_components.py
+++ home/templatetags/generic_components.py
@@ -135,12 +135,7 @@ def google_analytics(context, tracking_code=None, debug=False):
 
 
 @register.inclusion_tag('matomo_tracking.html')
-def matomo_tracking(global_site=False):
-    if global_site:
-        url = 'https://www.internetofgoodthings.org/'
-        site_id = globals_.site_settings.global_matomo_site_id
-    else:
-        url = globals_.site.root_url
-        site_id = globals_.site_settings.local_matomo_site_id
+def matomo_tracking(site_id):
+    url = globals_.site.root_url
 
     return {'site_id': site_id, 'url': url}
