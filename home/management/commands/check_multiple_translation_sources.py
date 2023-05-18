b3a1798e5d2016769a62742941006b2535c5a26d
Ali Hassan <alihassan01264@gmail.com>
2023-05-18 12:02:40 +0500

    refactor code and add more logs

--- home/management/commands/check_multiple_translation_sources.py
+++ home/management/commands/check_multiple_translation_sources.py
@@ -6,13 +6,13 @@ from wagtail_localize.models import TranslationSource
 class Command(BaseCommand):
     def handle(self, *args, **options):
         for page in Page.objects.all():
-            try:
-                TranslationSource.objects.get(
-                    object_id=page.translation_key,
-                    specific_content_type=page.content_type_id,
-                    translations__target_locale=page.locale,
-                )
-            except TranslationSource.DoesNotExist:
-                pass
-            except TranslationSource.MultipleObjectsReturned:
-                self.stdout.write(f"Multiple translation sources found for page:{page.id}, title:{page.title}")
+            translation_sources = TranslationSource.objects.filter(
+                object_id=page.translation_key,
+                specific_content_type=page.content_type_id,
+                translations__target_locale=page.locale,
+            )
+
+            if translation_sources.count() > 1:
+                self.stdout.write(f"Multiple translation sources found for page: {page.id}, title: {page.title}")
+                for translation_source in translation_sources:
+                    self.stdout.write(f" - Translation source: {translation_source.id}, locale: {translation_source.locale}")
