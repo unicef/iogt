8dfbe0944e2bd08ed7831e929f1aff296dd6463e
Ali Hassan <alihassan01264@gmail.com>
2023-05-25 21:47:03 +0500

    refactor code

--- matomo/templatetags/matomo_tags.py
+++ matomo/templatetags/matomo_tags.py
@@ -5,10 +5,14 @@ from django.conf import settings
 register = template.Library()
 
 
-@register.inclusion_tag('matomo_tracking_code.html', takes_context=True)
+@register.inclusion_tag('matomo_tracking_tags.html', takes_context=True)
 def matomo_tracking_tags(context):
     context.update({
-        'tracking_enabled': settings.MATOMO_TRACKING,
+        'tracking_enabled': (
+                settings.MATOMO_TRACKING
+                and settings.MATOMO_SERVER_URL
+                and settings.MATOMO_SITE_ID
+        ),
         'matomo_site_id': settings.MATOMO_SITE_ID,
         'matomo_server_url': settings.MATOMO_SERVER_URL
     })
