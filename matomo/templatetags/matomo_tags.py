d5443c214511e3c6d7b911bf9b7f370a242e54d9
Ian Stride <ianstride@gmail.com>
2023-10-12 17:40:46 +0100

    Create session-based user tracking ID
    When JavaScript is disabled in the browser, we use the Matomo Image Tracker to
    gather analytics data. In this case, first party cookies are also disabled and
    Matomo is not able to differentiate between different visitors very well. The
    tracking ID is intended to provide the same function as the visitor ID that is
    usually provided by Matomo when JS is enabled.

--- matomo/templatetags/matomo_tags.py
+++ matomo/templatetags/matomo_tags.py
@@ -1,26 +1,46 @@
+from uuid import uuid4
+
 from django import template
 from django.conf import settings
-
 
 register = template.Library()
 
 
 @register.inclusion_tag("matomo_tracking_tags.html", takes_context=True)
 def matomo_tracking_tags(context):
+    server_url = settings.MATOMO_SERVER_URL
+    site_id = settings.MATOMO_SITE_ID
+    uid = user_tracking_id(context["request"].session)
+
     context.update(
         {
-            "tracking_enabled": (
-                settings.MATOMO_TRACKING
-                and settings.MATOMO_SERVER_URL
-                and settings.MATOMO_SITE_ID
-            ),
-            "matomo_site_id": settings.MATOMO_SITE_ID,
-            "matomo_server_url": settings.MATOMO_SERVER_URL,
+            "tracking_enabled": (settings.MATOMO_TRACKING and server_url and site_id),
+            "matomo_site_id": site_id,
+            "matomo_server_url": server_url,
+            "matomo_image_tracker_url": image_tracker(server_url, site_id, uid),
         }
     )
 
     additional_site_id = settings.MATOMO_ADDITIONAL_SITE_ID
     if type(additional_site_id) is int and additional_site_id > 0:
-        context.update({"matomo_additional_site_id": additional_site_id})
+        context.update(
+            {
+                "matomo_additional_site_id": additional_site_id,
+                "matomo_additional_image_tracker_url": image_tracker(
+                    server_url, additional_site_id, uid
+                ),
+            }
+        )
 
     return context
+
+
+def user_tracking_id(session):
+    if not session.get("tid"):
+        session["tid"] = str(uuid4())
+
+    return session["tid"]
+
+
+def image_tracker(server_url, site_id, uid):
+    return f"{server_url}matomo.php?idsite={site_id}&rec=1&uid={uid}"
