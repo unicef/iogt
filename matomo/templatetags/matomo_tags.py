c0a527a0ae10ca2c7da17ee7592dde4620591d8c
Ian Stride <ianstride@gmail.com>
2023-11-01 13:43:59 +0000

    Generate a visitor ID instead of a user ID
    The user ID is supposed to identify a registered user of a site, but we are
    actually using it to identify a visitor. This change generates a visitor ID in a
    similar way to the Matomo JS client, for the particular case where the visitor
    does not have a JS-enabled browser. The user ID feature can then be implemented
    later on without coming into conflict with this visitor ID feature.

--- matomo/templatetags/matomo_tags.py
+++ matomo/templatetags/matomo_tags.py
@@ -1,8 +1,12 @@
 from uuid import uuid4
+from hashlib import sha1
+from datetime import datetime
 
 from django import template
 from django.conf import settings
 
+
+VISITOR_ID_KEY = "vid"
 register = template.Library()
 
 
@@ -10,15 +14,14 @@ register = template.Library()
 def matomo_tracking_tags(context):
     server_url = settings.MATOMO_SERVER_URL
     site_id = settings.MATOMO_SITE_ID
-    uid = user_tracking_id(context["request"].session)
+    vid = visitor_id(context["request"])
 
     context.update(
         {
             "tracking_enabled": (settings.MATOMO_TRACKING and server_url and site_id),
             "matomo_site_id": site_id,
             "matomo_server_url": server_url,
-            "matomo_image_tracker_url": image_tracker(server_url, site_id, uid),
-            "matomo_user_id": uid,
+            "matomo_image_tracker_url": image_tracker(server_url, site_id, vid),
         }
     )
 
@@ -28,7 +31,7 @@ def matomo_tracking_tags(context):
             {
                 "matomo_additional_site_id": additional_site_id,
                 "matomo_additional_image_tracker_url": image_tracker(
-                    server_url, additional_site_id, uid
+                    server_url, additional_site_id, vid
                 ),
             }
         )
@@ -36,12 +39,25 @@ def matomo_tracking_tags(context):
     return context
 
 
-def user_tracking_id(session):
-    if not session.get("tid"):
-        session["tid"] = str(uuid4())
+def visitor_id(request):
+    session = request.session
 
-    return session["tid"]
+    if not session.get(VISITOR_ID_KEY):
+        session[VISITOR_ID_KEY] = generate_random_id()
+
+    return session[VISITOR_ID_KEY]
 
 
-def image_tracker(server_url, site_id, uid):
-    return f"{server_url}matomo.php?idsite={site_id}&rec=1&uid={uid}"
+def generate_random_id():
+    return sha1(
+        "".join(
+            [
+                datetime.now().isoformat(),
+                str(uuid4()),
+            ]
+        ).encode("utf-8")
+    ).hexdigest()[0:16]
+
+
+def image_tracker(server_url, site_id, vid):
+    return f"{server_url}matomo.php?idsite={site_id}&rec=1&_id={vid}"
