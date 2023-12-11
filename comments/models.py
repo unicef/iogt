ac14e55900fdaccec2fa7841cc4027df5f67e7eb
Ian Stride <ianstride@gmail.com>
2023-12-11 11:18:48 +0000

    Format comments/models

--- comments/models.py
+++ comments/models.py
@@ -15,18 +15,18 @@ Moderator = get_comments_moderation_class()
 
 
 class CommentStatus:
-    OPEN = 'open'
-    CLOSED = 'closed'
-    DISABLED = 'disabled'
-    TIMESTAMPED = 'timestamped'
-    INHERITED = 'inherited'
+    OPEN = "open"
+    CLOSED = "closed"
+    DISABLED = "disabled"
+    TIMESTAMPED = "timestamped"
+    INHERITED = "inherited"
 
     Choices = (
-        (OPEN, 'Open'),
-        (CLOSED, 'Closed'),
-        (DISABLED, 'Disabled'),
-        (TIMESTAMPED, 'Timestamped'),
-        (INHERITED, 'Inherited'),
+        (OPEN, "Open"),
+        (CLOSED, "Closed"),
+        (DISABLED, "Disabled"),
+        (TIMESTAMPED, "Timestamped"),
+        (INHERITED, "Inherited"),
     )
 
 
@@ -45,33 +45,50 @@ class CommentableMixin(models.Model):
 
     Use comments_panels to modify commenting_status from the admin.
     """
-    commenting_status = models.CharField(max_length=15, choices=CommentStatus.Choices, default=CommentStatus.INHERITED)
+
+    commenting_status = models.CharField(
+        max_length=15,
+        choices=CommentStatus.Choices,
+        default=CommentStatus.INHERITED,
+    )
     commenting_starts_at = models.DateTimeField(null=True, blank=True)
     commenting_ends_at = models.DateTimeField(null=True, blank=True)
 
     comments_panels = [
-        FieldPanel('commenting_status', heading='Status'),
-        FieldPanel('commenting_starts_at', heading='Commenting Opens At'),
-        FieldPanel('commenting_ends_at', heading='Commenting Ends At')
-
+        FieldPanel("commenting_status", heading="Status"),
+        FieldPanel("commenting_starts_at", heading="Commenting Opens At"),
+        FieldPanel("commenting_ends_at", heading="Commenting Ends At"),
     ]
 
     def _get_valid_ancestor_for_commenting(self):
         from home.models import Section
 
-        ancestors = self.get_ancestors().type(Section).specific().order_by('-path')
+        ancestors = self.get_ancestors().type(Section).specific().order_by("-path")
         for ancestor in ancestors:
             if ancestor.commenting_status in [
-                CommentStatus.OPEN, CommentStatus.CLOSED, CommentStatus.DISABLED, CommentStatus.TIMESTAMPED]:
+                CommentStatus.OPEN,
+                CommentStatus.CLOSED,
+                CommentStatus.DISABLED,
+                CommentStatus.TIMESTAMPED,
+            ]:
                 return ancestor
 
     def should_show_comments_list(self):
         commenting_status = self.commenting_status
+
         if commenting_status == CommentStatus.INHERITED:
             ancestor = self._get_valid_ancestor_for_commenting()
-            commenting_status = ancestor.commenting_status if ancestor else CommentStatus.DISABLED
 
-        return commenting_status in [CommentStatus.OPEN, CommentStatus.CLOSED, CommentStatus.TIMESTAMPED]
+            if ancestor:
+                commenting_status = ancestor.commenting_status
+            else:
+                commenting_status = CommentStatus.DISABLED
+
+        return commenting_status in [
+            CommentStatus.OPEN,
+            CommentStatus.CLOSED,
+            CommentStatus.TIMESTAMPED,
+        ]
 
     def should_show_new_comment_box(self):
         commenting_still_valid = True
@@ -93,15 +110,18 @@ class CommentableMixin(models.Model):
         if commenting_starts_at:
             commenting_still_valid = commenting_starts_at < timezone.now()
         if commenting_ends_at:
-            commenting_still_valid = commenting_still_valid and commenting_ends_at > timezone.now()
+            commenting_still_valid = (
+                commenting_still_valid and commenting_ends_at > timezone.now()
+            )
 
-        return commenting_status == CommentStatus.OPEN or \
-               (commenting_status == CommentStatus.TIMESTAMPED and commenting_still_valid)
+        return commenting_status == CommentStatus.OPEN or (
+            commenting_status == CommentStatus.TIMESTAMPED and commenting_still_valid
+        )
 
     def get_absolute_url(self):
         if isinstance(self, Page):
             return self.get_url()
-        raise NotImplementedError('Implement get_absolute_url for your Model')
+        raise NotImplementedError("Implement get_absolute_url for your Model")
 
     class Meta:
         abstract = True
@@ -122,13 +142,13 @@ class CommunityCommentModeration(models.Model):
         max_length=255,
     )
     comment = models.OneToOneField(
-        to='django_comments_xtd.XtdComment',
-        related_name='comment_moderation',
+        to="django_comments_xtd.XtdComment",
+        related_name="comment_moderation",
         on_delete=models.CASCADE,
     )
 
     def __str__(self):
-        return f'{self.comment.id} | {self.state}'
+        return f"{self.comment.id} | {self.state}"
 
     class Meta:
         permissions = (
@@ -141,13 +161,13 @@ def comment_moderation_handler(sender, instance, created, **kwargs):
     if created:
         moderator = Moderator()
         instance.is_public = moderator.moderate(instance)
-        instance.save(update_fields=['is_public'])
+        instance.save(update_fields=["is_public"])
         CommunityCommentModeration.objects.create(comment_id=instance.id)
 
 
 @receiver(comment_was_flagged, sender=XtdComment)
 def comment_flagged(sender, comment, **kwargs):
-    if hasattr(comment, 'comment_moderation'):
+    if hasattr(comment, "comment_moderation"):
         comment_moderation = comment.comment_moderation
         comment_moderation.state = CommentModerationState.UNMODERATED
-        comment_moderation.save(update_fields=['state'])
+        comment_moderation.save(update_fields=["state"])
