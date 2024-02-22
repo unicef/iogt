9f2943958fc793e4d88bb098fd258df110f0b94a
Md Eamin Hossain <eamin.riseuplabs@gmail.com>
2024-02-22 14:37:06 +0600

    denial listing page done

--- cranky_uncle/urls.py
+++ cranky_uncle/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     # path('cranky-home/', views.cranky_uncle_home, name='cranky-home'),
     # path('cranky-home/', views.CrankyUncleHomeView.as_view(), name='cranky-home'),
     path('cranky-quiz/<slug:slug>/', views.CrankyUncleQuizView.as_view(), name='cranky-quiz'),
-    path('rapidpro-message-hook/', views.RapidProMessageHook.as_view(), name='rapidpro-message-hook')
+    path('rapidpro-message-hook/', views.RapidProMessageHook.as_view(), name='rapidpro-message-hook'),
+    
+    path("denial/<slug>/", views.DenialQuizView.as_view(), name="cranky_denial"),
+    path("denial/<slug>/<trigger_string>", views.DenialQuizSendMessageView.as_view(), name="denial_send_message"),
+    
 ]
