e5caa69344eb05917214aeceab9e10e5740a927c
Md Eamin Hossain <eamin.riseuplabs@gmail.com>
2024-03-23 22:10:21 +0600

    interactive page module added to admin panel

--- interactive/models.py
+++ interactive/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from wagtail.core.models import Page
+from wagtail.admin.edit_handlers import FieldPanel
+from home.mixins import PageUtilsMixin, TitleIconMixin
+from django.utils.translation import get_language
 
 class InteractiveChannel(models.Model):
     display_name = models.CharField(
@@ -30,3 +34,39 @@ class Message(models.Model):
 
     def __str__(self):
         return f'RapidPro {self.rapidpro_id}'
+
+class InteractivePage(Page, PageUtilsMixin, TitleIconMixin):
+    parent_page_types = [
+        "home.HomePage", "home.Section", 'home.FooterIndexPage'
+    ]
+    subpage_types = []
+    template = 'interactive/interactive_home.html'
+
+    button_text = models.CharField(max_length=255, null=True, blank=True)
+    trigger_string = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True,
+        help_text=_("Language short code will postfix after trigger string e.g string_en")
+        )
+    channel = models.ForeignKey(
+        InteractiveChannel,
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel('button_text'),
+        FieldPanel('trigger_string'),
+        FieldPanel('channel'),
+    ]
+    
+    def get_context(self, request):
+        context = super().get_context(request)
+        context['trigger_string_with_lang'] = self.trigger_string + '_' + get_language()
+
+        return context
+
+    def __str__(self):
+        return self.title
