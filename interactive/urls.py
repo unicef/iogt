d0bbbb71ea5fede348691311e5bd02f7ad562178
alviriseup <alvi@riseuplabs.com>
2024-05-05 14:07:28 +0600

    Added unit test for Interactive module

--- interactive/urls.py
+++ interactive/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 
 from interactive.views import InteractiveView
 
 app_name = 'interactive'
 
 urlpatterns = [
+    path('api/', include('interactive.api.urls')),
     path('<slug:slug>/', InteractiveView.as_view(), name='interactive_game'),
 ]
