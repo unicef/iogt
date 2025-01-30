f6fb0ebc216d5b552661358e99fc99e1a2093662
vimugdhabatra <vimugdha.rani@nagarro.com>
2025-01-30 06:18:55 -0800

    user authentication: restricting uninvited users to login

--- iogt_users/urls.py
+++ iogt_users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from iogt_users.views import UserDetailEditView, UserDetailView, InviteAdminUserView
 
 urlpatterns = [
-    # path('invite-admin-user/', InviteAdminUserView.as_view(), name='invite_admin_user'),
+    path('invite-admin-user/', InviteAdminUserView.as_view(), name='invite_admin_user'),
     path('profile/', UserDetailView.as_view(), name='user_profile'),
     path('profile/edit', UserDetailEditView.as_view(), name='user_profile_edit'),
 ]
