a9bea00eb90a62652f4c4af40c204ab187946e3c
Shubham Goel <130149461+shubhamgoel02@users.noreply.github.com>
2025-03-17 13:14:38 +0530

    Update urls.py

--- iogt/urls.py
+++ iogt/urls.py
@@ -53,13 +53,13 @@ urlpatterns = api_url_patterns + [
     path('admin/login/', AzureADSignupView.as_view(), name='azure_signup_view'),  # Override Wagtail admin login
     path('admin/', include(wagtailadmin_urls)),
     path('documents/', include(wagtaildocs_urls)),
-    #path('admin-login/', include(admin_login_urls), name='admin_login_urls'),
+    path('admin-login/', include(admin_login_urls), name='admin_login_urls'),
     *i18n_patterns(path('logout_hack_view', LogoutRedirectHackView.as_view(), name='logout_redirect')),
     *i18n_patterns(path('search/', search_views.search, name='search')),
     *i18n_patterns(path('users/', include(users_urls), name='users_urls')),
     *i18n_patterns(path('accounts/', include('allauth.urls'), name='allauth-urls')),
     *i18n_patterns(path('comments/', include('django_comments_xtd.urls'))),
-    *i18n_patterns(path('admin-login/', include('admin_login.urls'))),
+    #*i18n_patterns(path('admin-login/', include('admin_login.urls'))),
 
     path(
         'sw.js',
