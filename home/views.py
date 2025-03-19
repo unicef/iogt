6304bd07e9091bec5800ea5e7e995df79437df1b
vimugdhabatra <vimugdha.rani@nagarro.com>
2025-03-18 22:37:57 -0700

    offline functionality

--- home/views.py
+++ home/views.py
@@ -21,6 +21,7 @@ class ServiceWorkerView(TemplateView):
 def get_manifest(request):
     language = translation.get_language()
     manifest = get_object_or_404(ManifestSettings, language=language)
+    print(manifest.icon_96_96.title, "+++++++++++++++++++++++")
     response = {
         "name": manifest.name,
         "short_name": manifest.short_name,
@@ -34,17 +35,17 @@ def get_manifest(request):
         "icons": [
             {
                 "src": f"{manifest.icon_96_96.file.url}",
-                "type": f"image/{manifest.icon_96_96.title.split('.')[1]}",
+                "type": f"image/{manifest.icon_96_96.file.name.split('.')[-1]}",
                 "sizes": f"{manifest.icon_96_96.height}x{manifest.icon_96_96.width}",
             },
             {
                 "src": f"{manifest.icon_512_512.file.url}",
-                "type": f"image/{manifest.icon_512_512.title.split('.')[1]}",
+                "type": f"image/{manifest.icon_512_512.file.name.split('.')[-1]}",
                 "sizes": f"{manifest.icon_512_512.height}x{manifest.icon_512_512.width}",
             },
             {
                 "src": f"{manifest.icon_192_192.file.url}",
-                "type": f"image/{manifest.icon_192_192.title.split('.')[1]}",
+                "type": f"image/{manifest.icon_192_192.file.name.split('.')[-1]}",
                 "sizes": f"{manifest.icon_192_192.height}x{manifest.icon_192_192.width}",
                 "purpose": "any maskable",
             },
