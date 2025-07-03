3531c83dfdfdc40d841eac67ba8a8803342136b3
Ankit Chopra <ankit.chopra@nagarro.com>
2025-07-03 13:23:33 +0530

    added changes to add snippet to manage message and title

--- project_notifications/models.py
+++ project_notifications/models.py
@@ -1,5 +1,8 @@
 from django.db import models
-
+from wagtail.snippets.models import register_snippet
+from wagtail.admin.panels import FieldPanel
+from django.utils.timezone import now
+from wagtail.fields import RichTextField
 
 class Notification(models.Model):
     head = models.CharField(max_length=255)
@@ -10,3 +13,24 @@ class Notification(models.Model):
 
     def __str__(self):
         return self.head
+
+@register_snippet
+class SignupNotificationTemplate(models.Model):
+    title = models.CharField(max_length=255)
+    message = RichTextField(features=["bold", "italic", "link", "ul", "ol"])
+    active = models.BooleanField(default=True)
+    updated_at = models.DateTimeField(default=now)
+
+    panels = [
+        FieldPanel('title'),
+        FieldPanel('message'),
+        FieldPanel('active'),
+    ]
+
+    def __str__(self):
+        return f"{self.title} ({'active' if self.active else 'inactive'})"
+
+    class Meta:
+        verbose_name = "Signup Notification Template"
+        verbose_name_plural = "Signup Notification Templates"
+
