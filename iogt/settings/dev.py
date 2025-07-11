bdaf6d7bb3340f8cf11efa7f6f89032d3b3ca160
Ankit Chopra <ankit.chopra@nagarro.com>
2025-07-11 18:49:38 +0530

    changes of celery image and configuration

--- iogt/settings/dev.py
+++ iogt/settings/dev.py
@@ -29,6 +29,11 @@ DATABASES = {
         'PORT': getenv('DB_PORT', '5432'),
     }
 }
+CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+
 
 try:
     from .local import *
