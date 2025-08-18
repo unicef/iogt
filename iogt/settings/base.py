57c1b4fdd452a6afb665fb992a44d0825f9198b9
Ankit-lab809 <ankit.chopra@nagarro.com>
2025-08-18 15:49:50 +0530

    removed unnecessary code

--- iogt/settings/base.py
+++ iogt/settings/base.py
@@ -94,7 +94,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
     "iogt.middleware.LocaleMiddleware",
     "iogt.middleware.AdminLocaleMiddleware",
     'iogt.middleware.CustomRedirectMiddleware',
