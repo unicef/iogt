877524ccb068a352f875760f1cb485b922a1b2ed
Ankit-lab809 <ankit.chopra@nagarro.com>
2025-12-11 11:05:34 +0530

    fixed the article ancestor issue

--- home/models.py
+++ home/models.py
@@ -447,7 +447,7 @@ class AbstractArticle(Page, PageUtilsMixin, CommentableMixin, TitleIconMixin):
 
     @property
     def top_level_section(self):
-        return self.get_ancestors().filter(depth=4).first().specific
+        return self.get_ancestors().last().specific if self.get_ancestors() else None
 
     @property
     def offline_urls(self):
