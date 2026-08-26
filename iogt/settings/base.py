c04c9237b6fb990e2c4ac5d6f7d7d010c112b423
Gaurav Sachdeva <sachdeva.gaurav2911@gmail.com>
2026-08-26 13:05:22 +0530

    Adding auth.permission in Wagtail Transfer fields

--- iogt/settings/base.py
+++ iogt/settings/base.py
@@ -489,6 +489,7 @@ TRANSLATIONS_PROJECT_BASE_DIR = BASE_DIR
 WAGTAILTRANSFER_CHOOSER_API_PROXY_TIMEOUT=120
 
 WAGTAILTRANSFER_LOOKUP_FIELDS = {
+    'auth.permission': ['codename'],
     'contenttypes.contenttype': ['app_label', 'model'],
     'iogt_users.user': ['username'],
     'taggit.tag': ['slug'],
