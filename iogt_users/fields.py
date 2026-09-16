738fbb6cca7064dd2bb75f27bb1ce3ba439cbe52
Nagarro\ Prashant Upadhyay <prashant.upadhyay@nagarro.com>
2026-09-16 14:06:37 +0530

    User sign up field style changes

--- iogt_users/fields.py
+++ iogt_users/fields.py
@@ -4,6 +4,9 @@ from django.forms import TextInput
 
 class IogtPasswordField(PasswordField):
 
-    def __init__(self, label, autocomplete, **kwargs):
-        super().__init__(label=label, max_length=4, autocomplete=autocomplete,
-                         widget=TextInput(attrs={'type': 'number'}), **kwargs)
+    def __init__(self, label, autocomplete, css_class='', **kwargs):
+        super().__init__(label=label, max_length=4, autocomplete=autocomplete, **kwargs)
+        self.widget.attrs.update({
+            "class": css_class,
+            "type": "number"
+        })
